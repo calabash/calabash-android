c63d0c4d36e57aae7859f0f6debe53c1acf5dfdd
Jonas Maturana Larsen <jonasmaturanalarsen@gmail.com>
2012-02-06 14:37:17 +0100

    Added dump html

---
+++ instrumentation_backend/src/sh/calaba/instrumentationbackend/actions/webview/DumpBodyHtml.java
@@ -0,0 +1,31 @@
+package sh.calaba.instrumentationbackend.actions.webview;
+
+
+import sh.calaba.instrumentationbackend.Result;
+import sh.calaba.instrumentationbackend.actions.Action;
+import android.webkit.WebView;
+
+public class DumpBodyHtml implements Action {
+
+    @Override
+    public Result execute(String... args) {
+    	                         
+    	for (CalabashChromeClient ccc : CalabashChromeClient.findAndPrepareWebViews()) {
+    		WebView webView = ccc.getWebView();
+			
+			webView.loadUrl("javascript:(function() {" +
+				"prompt('calabash:' + document.body.innerHTML);" + 
+				"})()");
+
+			String r = ccc.getResult();
+			System.out.println("Html:");
+			System.out.println(r);
+		}
+    	return Result.successResult();
+    }
+
+    @Override
+    public String key() {
+        return "dump_body_html";
+    }
+}
