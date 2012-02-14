435d3c18e3097e5a48453c3f973b302dc1c017c7
Jonas Maturana Larsen <jonasmaturanalarsen@gmail.com>
2012-02-14 09:31:27 +0100

    Simulates the pressing the down key

---
+++ instrumentation_backend/src/sh/calaba/instrumentationbackend/actions/softkey/DownKey.java
@@ -0,0 +1,24 @@
+package sh.calaba.instrumentationbackend.actions.softkey;
+
+
+import com.jayway.android.robotium.solo.Solo;
+
+import sh.calaba.instrumentationbackend.InstrumentationBackend;
+import sh.calaba.instrumentationbackend.Result;
+import sh.calaba.instrumentationbackend.actions.Action;
+
+
+public class DownKey implements Action {
+
+    @Override
+    public Result execute(String... args) {
+        InstrumentationBackend.solo.sendKey(Solo.DOWN);
+        return Result.successResult();
+    }
+
+    @Override
+    public String key() {
+        return "send_key_down";
+    }
+
+}
