124daa1014718171c19e3a9fe2d6987f568fa24f
Edward Dale <edward.dale@simfy.com>
2012-02-21 15:31:56 +0100

    Clean up menu key pressing

--- instrumentation_backend/src/sh/calaba/instrumentationbackend/actions/softkey/PressMenu.java
+++ instrumentation_backend/src/sh/calaba/instrumentationbackend/actions/softkey/PressMenu.java
@@ -1,22 +1,26 @@
 package sh.calaba.instrumentationbackend.actions.softkey;
 
 
+import com.jayway.android.robotium.solo.Solo;
 import sh.calaba.instrumentationbackend.InstrumentationBackend;
 import sh.calaba.instrumentationbackend.Result;
+import sh.calaba.instrumentationbackend.TestHelpers;
 import sh.calaba.instrumentationbackend.actions.Action;
+import sh.calaba.instrumentationbackend.actions.Actions;
 
 
 public class PressMenu implements Action {
 
     @Override
     public Result execute(String... args) {
-        InstrumentationBackend.solo.clickOnMenuItem(args[0]);
+        Actions.parentInstrumentation.sendKeyDownUpSync(Solo.MENU);
+        TestHelpers.wait(1);
         return Result.successResult();
     }
 
     @Override
     public String key() {
-        return "select_from_menu";
+        return "press_menu";
     }
 
 }
