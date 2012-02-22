2900a6e6400b911a93624effcab7a102cde5e8df
Jonas Maturana Larsen <jonasmaturanalarsen@gmail.com>
2012-02-22 12:21:50 -0200

    Action that dumps innerHTML of the html tag - including head and body.

---
+++ instrumentation_backend/src/sh/calaba/instrumentationbackend/actions/webview/DumpHtml.java
@@ -0,0 +1,33 @@
+package sh.calaba.instrumentationbackend.actions.webview;
+
+
+import sh.calaba.instrumentationbackend.Result;
+import sh.calaba.instrumentationbackend.actions.Action;
+import android.webkit.WebView;
+
+public class DumpHtml implements Action {
+
+    @Override
+    public Result execute(String... args) {
+    	                         
+    	Result result = Result.successResult();
+    	for (CalabashChromeClient ccc : CalabashChromeClient.findAndPrepareWebViews()) {
+    		WebView webView = ccc.getWebView();
+			
+			webView.loadUrl("javascript:(function() {" +
+				"prompt('calabash:' + document.body.parentNode.innerHTML);" + 
+				"})()");
+
+			String r = ccc.getResult();
+			System.out.println("Html:");
+			System.out.println("" + r);
+			result.addBonusInformation(r);
+		}
+    	return result;
+    }
+
+    @Override
+    public String key() {
+        return "dump_html";
+    }
+}
