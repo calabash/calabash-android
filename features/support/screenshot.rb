9bce5c31a1431639cfbbc6374a34d629f60f7eca
Jonas Maturana Larsen <jonasmaturanalarsen@gmail.com>
2012-02-24 23:14:04 -0200

    Removing those annoying warnings about Step_index and Step_line

--- features/support/screenshot.rb
+++ features/support/screenshot.rb
@@ -1,5 +1,5 @@
 def take_screenshot
-  file_name = "#{ENV['SCREENSHOT_PATH_PREFIX']}screenshot_#{Step_line}.png"
+  file_name = "#{ENV['SCREENSHOT_PATH_PREFIX']}screenshot_#{StepCounter.step_line}.png"
   $stdout.puts "Taking screenshoot to #{file_name} from device: #{ENV['ADB_DEVICE_ARG']}"
   system("java -jar #{File.dirname(__FILE__)}/screenShotTaker.jar #{file_name} '#{ENV['ADB_DEVICE_ARG']}'")
 end
