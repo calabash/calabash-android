1581985ab4f1dd623a8a6403a48ac5340dab1835
Jonas Maturana Larsen <jonasmaturanalarsen@gmail.com>
2012-02-25 15:42:30 -0200

    Will now time out after 10 seconds if we cannot take screenshot

--- features/support/screenshot.rb
+++ features/support/screenshot.rb
@@ -1,5 +1,14 @@
 def take_screenshot
-  file_name = "#{ENV['SCREENSHOT_PATH_PREFIX']}screenshot_#{StepCounter.step_line}.png"
-  $stdout.puts "Taking screenshoot to #{file_name} from device: #{ENV['ADB_DEVICE_ARG']}"
-  system("java -jar #{File.dirname(__FILE__)}/screenShotTaker.jar #{file_name} '#{ENV['ADB_DEVICE_ARG']}'")
+  require 'timeout'
+  begin
+    Timeout.timeout(10) do
+      file_name = "#{ENV['SCREENSHOT_PATH_PREFIX']}screenshot_#{StepCounter.step_line}.png"
+      $stdout.puts "Taking screenshoot to #{file_name} from device: #{ENV['ADB_DEVICE_ARG']}"
+      cmd = "java -jar #{File.dirname(__FILE__)}/screenShotTaker.jar #{file_name} #{ENV['ADB_DEVICE_ARG']}"
+      $stdout.puts cmd
+      puts `#{cmd}`
+    end
+  rescue Timeout::Error
+    raise Exception, "take_screenshot timed out"
+  end
 end
