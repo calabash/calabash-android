c57dca103b3499347a4d7ce585f1a01b953014ee
Jonas Maturana Larsen <jonasmaturanalarsen@gmail.com>
2012-02-24 23:11:46 -0200

    Preparing screenshotTaker to be using ADB_DEVICE_ARG

--- features/support/screenshot.rb
+++ features/support/screenshot.rb
@@ -1,5 +1,5 @@
 def take_screenshot
   file_name = "#{ENV['SCREENSHOT_PATH_PREFIX']}screenshot_#{Step_line}.png"
-  $stdout.puts "Taking screenshoot to #{file_name} from device: #{ENV['DEVICE_SERIAL']}"
-  system("java -jar #{File.dirname(__FILE__)}/screenShotTaker.jar #{file_name} #{ENV['DEVICE_SERIAL']}")
+  $stdout.puts "Taking screenshoot to #{file_name} from device: #{ENV['ADB_DEVICE_ARG']}"
+  system("java -jar #{File.dirname(__FILE__)}/screenShotTaker.jar #{file_name} '#{ENV['ADB_DEVICE_ARG']}'")
 end
