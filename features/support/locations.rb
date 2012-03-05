8e685723f4603847f77e30bcca971f1afe6abf78
Jonas Maturana Larsen <jonasmaturanalarsen@gmail.com>
2012-03-04 22:40:33 -0300

    moved require of geocoder gem so it is only need with using the Geocoder functionality

--- features/support/locations.rb
+++ features/support/locations.rb
@@ -1,12 +1,12 @@
-#require 'geocoder'
-#def set_gps_coordinates_from_location(location)
-#  results = Geocoder.search(location)
-#  raise Exception, "Got no results for #{location}" if results.empty?
-#  
-#  best_result = results.first
-#  set_gps_coordinates(best_result.latitude, best_result.longitude)
-#end
-#
+def set_gps_coordinates_from_location(location)
+  require 'geocoder'
+  results = Geocoder.search(location)
+  raise Exception, "Got no results for #{location}" if results.empty?
+  
+  best_result = results.first
+  set_gps_coordinates(best_result.latitude, best_result.longitude)
+end
+
 def set_gps_coordinates(latitude, longitude)
   performAction('set_gps_coordinates', latitude, longitude) 
 end
