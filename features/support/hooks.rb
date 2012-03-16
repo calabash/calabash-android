d21005d200061a92076ed7f25324135342be518e
Jonas Maturana Larsen <jonasmaturanalarsen@gmail.com>
2012-03-16 16:07:46 -0300

    Added hints how to run cucumber without ant to the output

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -50,6 +50,19 @@ AfterStep do |scenario|
   StepCounter.step_line = scenario.raw_steps[StepCounter.step_index].line unless scenario.raw_steps[StepCounter.step_index].nil?
 end
 
+at_exit do
+  $stdout.puts "Hit: You can run cucumber directly using this command:"
+  cucumber_cmd = "PACKAGE_NAME=#{ENV["PACKAGE_NAME"]} TEST_PACKAGE_NAME=#{ENV["TEST_PACKAGE_NAME"]} APP_PATH=#{ENV["APP_PATH"]} TEST_APP_PATH=#{ENV["TEST_APP_PATH"]} TEST_SERVER_PORT=#{ENV["TEST_SERVER_PORT"]} ADB_DEVICE_ARG=#{ENV["ADB_DEVICE_ARG"]} cucumber"
+  $stdout.puts cucumber_cmd
+  features = Dir["features/*.feature"]
+  unless features.empty?
+    $stdout.puts "To only run #{features.first}:"
+    puts "#{cucumber_cmd} #{features.first}"
+  end
+end
+
+
+
 StepCounter = Class.new
 class << StepCounter
   @step_index = 0
