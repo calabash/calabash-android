92a28c1049b2662f878a4924f5a9edf1672fb47d
Jonas Maturana Larsen <jonasmaturanalarsen@gmail.com>
2012-03-29 22:41:40 -0300

    Increased timeout for screenshots to 30 seconds

--- features/support/screenshot.rb
+++ features/support/screenshot.rb
@@ -1,7 +1,7 @@
 def take_screenshot
   require 'timeout'
   begin
-    Timeout.timeout(10) do
+    Timeout.timeout(30) do
       file_name = "#{ENV['SCREENSHOT_PATH_PREFIX']}screenshot_#{StepCounter.step_line}.png"
       log "Taking screenshoot to #{file_name} from device: #{ENV['ADB_DEVICE_ARG']}"
       system("java -jar #{File.dirname(__FILE__)}/screenShotTaker.jar #{file_name} #{ENV['ADB_DEVICE_ARG']}")
