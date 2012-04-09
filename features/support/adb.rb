ff0224bed87177d5788264d2b379d85e55a7846a
Jonas Maturana Larsen <jonasmaturanalarsen@gmail.com>
2012-04-09 15:25:56 -0300

    Making Calabash-android Windows friendly. Lots of testing to do

--- features/support/adb.rb
+++ features/support/adb.rb
@@ -1,3 +1,11 @@
 def adb_command
-  "#{ENV['ANDROID_HOME']}/platform-tools/adb #{ENV["ADB_DEVICE_ARG"]}"
+  if is_windows?
+    %Q("#{ENV["ANDROID_HOME"]}\\platform-tools\\adb.exe" #{ENV["ADB_DEVICE_ARG"]})
+  else
+    %Q(#{ENV["ANDROID_HOME"]}/platform-tools/adb #{ENV["ADB_DEVICE_ARG"]})
+  end
+end
+
+def is_windows?
+   ENV["OS"] == "Windows_NT"
 end
