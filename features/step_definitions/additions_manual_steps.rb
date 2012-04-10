08c2740ba7703eb0a495b05036a9730f43025e7a
gw111zz <gw111zz@gmail.com>
2012-04-10 10:33:52 +0100

    Correct spelling mistake Rename manual_steps.rb to have additions_ prefix to make merging easier

--- features/step_definitions/additions_manual_steps.rb
+++ features/step_definitions/additions_manual_steps.rb
@@ -1,4 +1,4 @@
-Then /^I manually compare the current screen with the reference image "([^\"]*)"$/ do |name|
+Then /^I compare the current screen with the reference image "([^\"]*) manually"$/ do |name|
     # TODO: once test artifacts directory is configurable, the image location
     # will need to be either a fully qualified path somewhere (webserver?) or
     # be relative to the output HTML file
