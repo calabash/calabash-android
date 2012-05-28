6bf3bbe39c05fa56f8eaa5c9cfdb7c584bf275b2
Jonas Maturana Larsen <jonasmaturanalarsen@gmail.com>
2012-05-28 13:21:37 -0300

    added EnterKey action

---
+++ ruby-gem/test-server/instrumentation-backend/src/sh/calaba/instrumentationbackend/actions/softkey/EnterKey.java
@@ -0,0 +1,24 @@
+package sh.calaba.instrumentationbackend.actions.softkey;
+
+
+import com.jayway.android.robotium.solo.Solo;
+
+import sh.calaba.instrumentationbackend.InstrumentationBackend;
+import sh.calaba.instrumentationbackend.Result;
+import sh.calaba.instrumentationbackend.actions.Action;
+
+
+public class EnterKey implements Action {
+
+    @Override
+    public Result execute(String... args) {
+        InstrumentationBackend.solo.sendKey(Solo.ENTER);
+        return Result.successResult();
+    }
+
+    @Override
+    public String key() {
+        return "send_key_enter";
+    }
+
+}
