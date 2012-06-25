c5468e83a8eda30288f3118861c72f8b71a4dcca
Jonas Maturana Larsen <jonasmaturanalarsen@gmail.com>
2012-06-25 16:00:02 -0300

    Created Client class that represents a device. This refactoring is done to allow Calabash to interact with more than one device in the	same test.

--- ruby-gem/lib/calabash-android/operations.rb
+++ ruby-gem/lib/calabash-android/operations.rb
@@ -15,19 +15,7 @@ module Operations
   end
 
   def take_screenshot
-    path = ENV["SCREENSHOT_PATH_PREFIX"] || "results"
-    FileUtils.mkdir_p path unless File.exist? path
-    filename_prefix = FeatureNameMemory.feature_name.gsub(/\s+/, '_').downcase
-    begin
-      Timeout.timeout(30) do
-        file_name = "#{path}/#{filename_prefix}_#{StepCounter.step_line}.png"
-        log "Taking screenshoot to #{file_name} from device: #{ENV['ADB_DEVICE_ARG']}"
-        system("java -jar #{File.dirname(__FILE__)}/lib/screenShotTaker.jar #{file_name} #{ENV['ADB_DEVICE_ARG']}")
-        log "Screenshot stored in: #{file_name}"
-      end
-    rescue Timeout::Error
-      raise Exception, "take_screenshot timed out"
-    end
+    Client.default_client.take_screenshot
   end
 
   def macro(txt)
@@ -38,30 +26,8 @@ module Operations
     end
   end
 
-  def performAction(action, *arguments)
-    log "Action: #{action} - Params: #{arguments.join(', ')}"
-
-    action = {"command" => action, "arguments" => arguments}
-
-    Timeout.timeout(300) do
-      begin
-        @@client.send(action.to_json + "\n", 0) #force_encoding('UTF-8') seems to be missing from JRuby
-        result = @@client.readline
-      rescue Exception => e
-        log "Error communicating with test server: #{e}"
-        raise e
-      end
-      log "Result:'" + result.strip + "'"
-      raise "Empty result from TestServer" if result.chomp.empty?
-      result = JSON.parse(result)
-      if not result["success"] then
-        take_screenshot
-        raise result["message"].to_s
-      end
-      return result
-    end
-  rescue Timeout::Error
-    raise Exception, "Step timed out"
+  def performAction(action, arguments)
+    Client.default_client.perform_action(action, arguments)
   end
 
   def wait_for(timeout, &block)
@@ -93,16 +59,6 @@ module Operations
     raise "Not yet implemented."
   end
 
-
-  ##adb
-  def adb_command
-    if is_windows?
-      %Q("#{ENV["ANDROID_HOME"]}\\platform-tools\\adb.exe" #{ENV["ADB_DEVICE_ARG"]})
-    else
-      %Q(#{ENV["ANDROID_HOME"]}/platform-tools/adb #{ENV["ADB_DEVICE_ARG"]})
-    end
-  end
-
   def is_windows?
      ENV["OS"] == "Windows_NT"
   end
@@ -110,41 +66,113 @@ module Operations
 
   ### app life cycle
   def connect_to_test_server
-    log `#{adb_command} forward tcp:#{ENV["TEST_SERVER_PORT"]} tcp:7101`
-
-    end_time = Time.now + 60
-    begin
-      Timeout.timeout(10) do
-        @@client = TCPSocket.open('127.0.0.1',ENV["TEST_SERVER_PORT"])
-        @@client.send("Ping!\n",0)
-        log "Got '#{@@client.readline.strip}' from testserver"
-      end
-    rescue Exception => e
-      log "Got exception:#{e}. Retrying!"
-      sleep(1)
-      retry unless Time.now > end_time
-    end
+    puts "Explicit calls to connect_to_test_server should be removed."
+    puts "Please take a look in your hooks file for calls to this methods."
+    puts "(Hooks are stored in features/support)")
   end
 
   def disconnect_from_test_server
-    log "Closing connection to test"
-    @@client.close
+    puts "Explicit calls to disconnect_from_test_server should be removed."
+    puts "Please take a look in your hooks file for calls to this methods."
+    puts "(Hooks are stored in features/support)")
   end
 
 
+  class Client
+    @@default_client = nil
 
-  ##location
-  def set_gps_coordinates_from_location(location)
-    require 'geocoder'
-    results = Geocoder.search(location)
-    raise Exception, "Got no results for #{location}" if results.empty?
+    def self.default_client
+      @@default_client = Client.new(ENV["ADB_DEVICE_ARG"], ENV["TEST_SERVER_PORT"]) unless @@default_client
+      @@default_client
+    end
 
-    best_result = results.first
-    set_gps_coordinates(best_result.latitude, best_result.longitude)
-  end
+    def initialize(serial, server_port)
 
-  def set_gps_coordinates(latitude, longitude)
-    performAction('set_gps_coordinates', latitude, longitude)
+      @serial = serial
+      log `#{adb_command} forward tcp:#{server_port} tcp:7101`
+      sleep 2
+      @connection = TCPSocket.open('127.0.0.1', server_port)
+
+      end_time = Time.now + 60
+      begin
+        Timeout.timeout(10) do
+          @connection.send("Ping!\n",0)
+          log "Got '#{@connection.readline.strip}' from testserver"
+        end
+      rescue Exception => e
+        log "Got exception:#{e}. Retrying!"
+        sleep(1)
+        retry unless Time.now > end_time
+      end
+    end
+
+    def perform_action(action, *arguments)
+      log "Action: #{action} - Params: #{arguments.join(', ')}"
+
+      action = {"command" => action, "arguments" => arguments}
+
+      Timeout.timeout(300) do
+        begin
+          @connection.send(action.to_json + "\n", 0) #force_encoding('UTF-8') seems to be missing from JRuby
+          result = @connection.readline
+        rescue Exception => e
+          log "Error communicating with test server: #{e}"
+          raise e
+        end
+        log "Result:'" + result.strip + "'"
+        raise "Empty result from TestServer" if result.chomp.empty?
+        result = JSON.parse(result)
+        if not result["success"] then
+          take_screenshot
+          raise result["message"].to_s
+        end
+        return result
+      end
+    rescue Timeout::Error
+      raise Exception, "Step timed out"
+    end
+
+    def take_screenshot
+      path = ENV["SCREENSHOT_PATH_PREFIX"] || "results"
+      FileUtils.mkdir_p path unless File.exist? path
+      filename_prefix = FeatureNameMemory.feature_name.gsub(/\s+/, '_').downcase
+      begin
+        Timeout.timeout(30) do
+          file_name = "#{path}/#{filename_prefix}_#{StepCounter.step_line}.png"
+          log "Taking screenshoot to #{file_name} from device: #{ENV['ADB_DEVICE_ARG']}"
+          system("java -jar #{File.dirname(__FILE__)}/lib/screenShotTaker.jar #{file_name} #{ENV['ADB_DEVICE_ARG']}")
+          log "Screenshot stored in: #{file_name}"
+        end
+      rescue Timeout::Error
+        raise Exception, "take_screenshot timed out"
+      end
+    end
+
+    def adb_command
+      if is_windows?
+        %Q("#{ENV["ANDROID_HOME"]}\\platform-tools\\adb.exe" #{@serial})
+      else
+        %Q(#{ENV["ANDROID_HOME"]}/platform-tools/adb #{@serial})
+      end
+    end
+
+    def log(message)
+      $stdout.puts "#{Time.now.strftime("%Y-%m-%d %H:%M:%S")} - #{message}" if (ARGV.include? "-v" or ARGV.include? "--verbose")
+    end
+
+    ##location
+    def set_gps_coordinates_from_location(location)
+      require 'geocoder'
+      results = Geocoder.search(location)
+      raise Exception, "Got no results for #{location}" if results.empty?
+
+      best_result = results.first
+      set_gps_coordinates(best_result.latitude, best_result.longitude)
+    end
+
+    def set_gps_coordinates(latitude, longitude)
+      perform_action('set_gps_coordinates', latitude, longitude)
+    end
   end
 
 
