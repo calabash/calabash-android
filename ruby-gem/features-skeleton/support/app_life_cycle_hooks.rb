4c91b0aefbaca0caf404d3708171e8528be3d7d1
Jonas Maturana Larsen <jonasmaturanalarsen@gmail.com>
2012-06-25 20:08:42 -0300

    Refactoring a little bit

--- ruby-gem/features-skeleton/support/app_life_cycle_hooks.rb
+++ ruby-gem/features-skeleton/support/app_life_cycle_hooks.rb
@@ -3,26 +3,5 @@ require 'calabash-android/management/adb'
 Before do |scenario|
 
   return if scenario.failed? #No need to start the server is anything before this has failed.
-  cmd = "#{adb_command} shell am instrument -w -e class sh.calaba.instrumentationbackend.InstrumentationBackend #{ENV['TEST_PACKAGE_NAME']}/sh.calaba.instrumentationbackend.CalabashInstrumentationTestRunner"
-  log "Starting test server using:"
-  log cmd
-  if is_windows?
-    system(%Q(start /MIN cmd /C #{cmd}))
-  else
-    `#{cmd} 1>&2 &`
-  end
-  
-  sleep 2
-  begin
-    connect_to_test_server
-    log "Connection established"
-  rescue Exception => e
-    log "Exception:#{e.backtrace}"
-  end
-end
-
-
-
-After do |scenario| 
-  disconnect_from_test_server
+  start_test_server_in_background
 end
--- ruby-gem/lib/calabash-android/management/adb.rb
+++ ruby-gem/lib/calabash-android/management/adb.rb
@@ -9,3 +9,14 @@ end
 def is_windows?
    ENV["OS"] == "Windows_NT"
 end
+
+def start_test_server_in_background
+  cmd = "#{adb_command} shell am instrument -w -e class sh.calaba.instrumentationbackend.InstrumentationBackend #{ENV['TEST_PACKAGE_NAME']}/sh.calaba.instrumentationbackend.CalabashInstrumentationTestRunner"
+  log "Starting test server using:"
+  log cmd
+  if is_windows?
+    system(%Q(start /MIN cmd /C #{cmd}))
+  else
+    `#{cmd} 1>&2 &`
+  end
+end
