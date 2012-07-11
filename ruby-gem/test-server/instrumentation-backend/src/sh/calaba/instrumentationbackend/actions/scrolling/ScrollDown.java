7bb2fdcba6e78c140c7ee56f44a75bcf6bad424a
Jeffrey S. Morgan <jeff.morgan@leandog.com>
2012-07-11 10:09:01 -0400

    fixed problems with scroll_down and scroll_up - changed to use Robotium

--- ruby-gem/test-server/instrumentation-backend/src/sh/calaba/instrumentationbackend/actions/scrolling/ScrollDown.java
+++ ruby-gem/test-server/instrumentation-backend/src/sh/calaba/instrumentationbackend/actions/scrolling/ScrollDown.java
@@ -4,19 +4,13 @@ package sh.calaba.instrumentationbackend.actions.scrolling;
 import sh.calaba.instrumentationbackend.InstrumentationBackend;
 import sh.calaba.instrumentationbackend.Result;
 import sh.calaba.instrumentationbackend.actions.Action;
-import sh.calaba.instrumentationbackend.actions.Actions;
-import android.test.TouchUtils;
 
 
 public class ScrollDown implements Action {
 
     @Override
     public Result execute(String... args) {
-        TouchUtils.dragQuarterScreenUp(Actions.parentTestCase, InstrumentationBackend.solo.getCurrentActivity());
-        TouchUtils.dragQuarterScreenUp(Actions.parentTestCase, InstrumentationBackend.solo.getCurrentActivity());
-        TouchUtils.dragQuarterScreenUp(Actions.parentTestCase, InstrumentationBackend.solo.getCurrentActivity());
-        TouchUtils.dragQuarterScreenUp(Actions.parentTestCase, InstrumentationBackend.solo.getCurrentActivity());
-
+    	InstrumentationBackend.solo.scrollDown();
         return Result.successResult();
     }
 
