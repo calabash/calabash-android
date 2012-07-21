0498663e8dbc9548cb7513dcf5f4e8ed3c2658c9
Jeffrey S. Morgan <jeff.morgan@leandog.com>
2012-07-20 21:52:34 -0400

    added Action to enter text into EditText identified by id

---
+++ ruby-gem/test-server/instrumentation-backend/src/sh/calaba/instrumentationbackend/actions/text/EnterTextById.java
@@ -0,0 +1,30 @@
+package sh.calaba.instrumentationbackend.actions.text;
+
+import android.view.View;
+import android.widget.EditText;
+import sh.calaba.instrumentationbackend.InstrumentationBackend;
+import sh.calaba.instrumentationbackend.Result;
+import sh.calaba.instrumentationbackend.TestHelpers;
+import sh.calaba.instrumentationbackend.actions.Action;
+
+public class EnterTextById implements Action {
+
+	@Override
+	public Result execute(String... args) {
+        final View view = TestHelpers.getViewById(args[0]);
+        if(view == null) {
+            return new Result(false, "No view found with id: '" + args[1] + "'");
+        } else if (!(view instanceof EditText)) {
+            return new Result(false, "Expected EditText found: '" + view.getClass() + "'");
+        } else {
+            InstrumentationBackend.solo.enterText((EditText)view, args[0]);
+            return Result.successResult();
+        }
+	}
+
+	@Override
+	public String key() {
+		return "enter_text_into_id_field";
+	}
+
+}
