d97abfdc55f89bb823ce959a0587c18ed63f7099
Jeffrey S. Morgan <jeff.morgan@leandog.com>
2012-07-20 22:25:08 -0400

    verified ClearTextById

--- ruby-gem/test-server/instrumentation-backend/src/sh/calaba/instrumentationbackend/actions/text/ClearTextById.java
+++ ruby-gem/test-server/instrumentation-backend/src/sh/calaba/instrumentationbackend/actions/text/ClearTextById.java
@@ -11,10 +11,9 @@ public class ClearTextById implements Action {
 
 	@Override
 	public Result execute(String... args) {
-        String contentDescription = args[0];
-        final View view = TestHelpers.getViewById(args[1]);
+        final View view = TestHelpers.getViewById(args[0]);
         if(view == null) {
-            return new Result(false, "No view found with id: '" + contentDescription + "'");
+            return new Result(false, "No view found with id: '" + args[0] + "'");
         } else if (!(view instanceof EditText)) {
             return new Result(false, "Expected EditText found: '" + view.getClass() + "'");
         } else {
