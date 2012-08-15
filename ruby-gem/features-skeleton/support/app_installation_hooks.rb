0160bc21584492b4c831343d6d1adf97473e8e8a
Nicholas Albion <nicholas_albion@rta.nsw.gov.au>
2012-08-15 10:52:39 +1000

    Applied John Gallagher's scenario outline fix. Added "invocation" attribute to FeatureNameMemory to support screenshots for scenario outlines.

--- ruby-gem/features-skeleton/support/app_installation_hooks.rb
+++ ruby-gem/features-skeleton/support/app_installation_hooks.rb
@@ -5,7 +5,12 @@ AfterConfiguration do |config|
 end
 
 Before do |scenario|
-  feature_name = scenario.feature.name
+  @scenario_is_outline = (scenario.class == Cucumber::Ast::OutlineTable::ExampleRow)
+  if @scenario_is_outline 
+    scenario = scenario.scenario_outline 
+  end 
+
+  feature_name = scenario.feature.title
   if FeatureNameMemory.feature_name != feature_name \
       or ENV["RESET_BETWEEN_SCENARIOS"] == "1"
     if ENV["RESET_BETWEEN_SCENARIOS"] == "1"
@@ -18,11 +23,14 @@ Before do |scenario|
     install_app(ENV["TEST_APP_PATH"])
     install_app(ENV["APP_PATH"])
     FeatureNameMemory.feature_name = feature_name
-	end
+	FeatureNameMemory.invocation = 1
+  else
+    FeatureNameMemory.invocation += 1
+  end
 end
 
 FeatureNameMemory = Class.new
 class << FeatureNameMemory
   @feature_name = nil
-  attr_accessor :feature_name
+  attr_accessor :feature_name, :invocation
 end
