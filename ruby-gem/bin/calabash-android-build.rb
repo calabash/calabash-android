a16de693ddf0fc4ccd028e07ce2c79a43317acd6
Matt Langreder <withoutclass@gmail.com>
2012-08-21 09:43:59 -0700

    Converting configuration over to YAML and modifying the gem files to use the new config

--- ruby-gem/bin/calabash-android-build.rb
+++ ruby-gem/bin/calabash-android-build.rb
@@ -1,30 +1,28 @@
 def calabash_build(app)
 
-
-
-  keystore = read_keystore_info()
+  config = get_config()
 
   test_server_template_dir = File.join(File.dirname(__FILE__), '..', 'test-server')
-  
+
   Dir.mktmpdir do |workspace_dir|
-    
+
     @test_server_dir = File.join(workspace_dir, 'test-server')
     FileUtils.cp_r(test_server_template_dir, workspace_dir)
-    
+
     ant_executable = (is_windows? ? "ant.bat" : "ant")
     Dir.chdir(@test_server_dir) {
       args = [
         ant_executable,
-        "clean", 
+        "clean",
         "package",
-        "-Dtested.package_name=#{package_name(app)}",
-        "-Dtested.main_activity=#{main_activity(app)}",
-        "-Dtested.project.apk=\"#{app}\"",
-        "-Dandroid.api.level=#{api_level}",
-        "-Dkey.store=\"#{File.expand_path keystore["keystore_location"]}\"",
-        "-Dkey.store.password=#{keystore["keystore_password"]}",
-        "-Dkey.alias=#{keystore["keystore_alias"]}",
-        "-Dkey.alias.password=#{keystore["keystore_alias_password"]}",
+        "-Dtested.package_name=\"#{config['package_name']}\"",
+        "-Dtested.main_activity=#{config['activity_name']}",
+        "-Dtested.project.apk=\"#{config['app_path']}\"",
+        "-Dandroid.api.level=#{config['api_level']}",
+        "-Dkey.store=\"#{File.expand_path config['keystore_location']}\"",
+        "-Dkey.store.password=#{config['keystore_password']}",
+        "-Dkey.alias=#{config['keystore_alias']}",
+        "-Dkey.alias.password=#{config['keystore_alias_password']}"
       ]
       STDOUT.sync = true
       IO.popen(args.join(" ")) do |io|
@@ -42,18 +40,5 @@ def calabash_build(app)
     test_server_file_name = test_server_path(app)
     FileUtils.cp(test_apk, test_server_file_name)
     puts "Done building the test server. Moved it to #{test_server_file_name}"
-  end
-end
-
-def read_keystore_info
-  if File.exist? ".calabash_settings"
-    JSON.parse(IO.read(".calabash_settings"))
-  else
-    {
-    "keystore_location" => "#{ENV["HOME"]}/.android/debug.keystore",
-    "keystore_password" => "android",
-    "keystore_alias" => "androiddebugkey",
-    "keystore_alias_password" => "android"
-    }
   end
 end
