1ddcfd3a746b91c2f9157ce23cbf603a95a1eca5
Jonas Maturana Larsen <jonasmaturanalarsen@gmail.com>
2012-09-28 21:33:12 +0200

    Version 0.3.0.pre6

--- ruby-gem/bin/calabash-android-build.rb
+++ ruby-gem/bin/calabash-android-build.rb
@@ -10,9 +10,13 @@ def calabash_build(app)
       FileUtils.cp(unsigned_test_apk, "TestServer.apk")
       FileUtils.cp(File.join(File.dirname(__FILE__), '..', 'test-server/AndroidManifest.xml'), "AndroidManifest.xml")
 
-      system  %Q{ruby -pi.bak -e "gsub(/#targetPackage#/, '#{package_name(app)}')" AndroidManifest.xml}
-      
-      system("#{ENV["ANDROID_HOME"]}/platform-tools/aapt package -M AndroidManifest.xml  -I #{android_platform}/android.jar -F dummy.apk")
+      unless system  %Q{ruby -pi.bak -e "gsub(/#targetPackage#/, '#{package_name(app)}')" AndroidManifest.xml}
+        raise "Could not replace package name in manifest"
+      end
+
+      unless system("#{ENV["ANDROID_HOME"]}/platform-tools/aapt package -M AndroidManifest.xml  -I #{android_platform}/android.jar -F dummy.apk")
+        raise "Could not create dummy.apk"
+      end
 
       Zip::ZipFile.new("dummy.apk").extract("AndroidManifest.xml","customAndroidManifest.xml")
       Zip::ZipFile.open("TestServer.apk") do |zip_file|
@@ -20,7 +24,9 @@ def calabash_build(app)
       end
     end
     cmd = "jarsigner -sigalg MD5withRSA -digestalg SHA1 -signedjar #{test_server_file_name} -storepass #{keystore["keystore_password"]} -keystore \"#{File.expand_path keystore["keystore_location"]}\" #{workspace_dir}/TestServer.apk #{keystore["keystore_alias"]}"
-    system(cmd)
+    unless system(cmd)
+      raise "Could not sign test server"
+    end
   end
   puts "Done signing the test server. Moved it to #{test_server_file_name}"
 end
