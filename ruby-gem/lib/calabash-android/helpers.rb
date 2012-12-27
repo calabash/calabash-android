b24607bacb8689703f78d7d2dc1744c4659bb703
Jonas Maturana Larsen <jonasmaturanalarsen@gmail.com>
2012-12-27 11:42:23 +0100

    Making calabash-android work with the French output that keytool can output

--- ruby-gem/lib/calabash-android/helpers.rb
+++ ruby-gem/lib/calabash-android/helpers.rb
@@ -116,7 +116,7 @@ def fingerprint_from_apk(app_path)
 end
 
 def extract_md5_fingerprint(fingerprints)
-  m = fingerprints.scan(/MD5:\s+((?:\h\h:){15}\h\h)/).flatten
+  m = fingerprints.scan(/MD5\s*:\s*((?:\h\h:){15}\h\h)/).flatten
   raise "No MD5 fingerprint found:\n #{fingerprints}" if m.empty?
   m.first
 end
