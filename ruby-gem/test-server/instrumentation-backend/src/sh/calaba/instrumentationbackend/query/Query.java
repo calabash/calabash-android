d5eec3f896edb645428fecd0e04a0d58a0f62d5b
Karl Krukow <karl.krukow@gmail.com>
2012-12-28 20:46:40 +0100

    reformat code, and dont kill app on error

--- ruby-gem/test-server/instrumentation-backend/src/sh/calaba/instrumentationbackend/query/Query.java
+++ ruby-gem/test-server/instrumentation-backend/src/sh/calaba/instrumentationbackend/query/Query.java
@@ -16,69 +16,76 @@ import android.view.View;
 
 public class Query {
 
-    private String queryString;
+	private String queryString;
 	@SuppressWarnings("rawtypes")
 	private List arguments;
 
-    public Query(String queryString) {        
-        this.queryString = queryString;
-        this.arguments = Collections.EMPTY_LIST;
-        if (this.queryString == null || this.queryString.trim().equals("")) 
-        {
-        	throw new IllegalArgumentException("Illegal query: "+this.queryString);
-        }
-    }
+	public Query(String queryString) {
+		this.queryString = queryString;
+		this.arguments = Collections.EMPTY_LIST;
+		if (this.queryString == null || this.queryString.trim().equals("")) {
+			throw new IllegalArgumentException("Illegal query: "
+					+ this.queryString);
+		}
+	}
 
-    @SuppressWarnings("rawtypes")
-	public Query(String queryString,List args) {
-        this(queryString);
-        this.arguments = args;
-    }
+	@SuppressWarnings("rawtypes")
+	public Query(String queryString, List args) {
+		this(queryString);
+		this.arguments = args;
+	}
 
-    @SuppressWarnings("rawtypes")
-	public List executeInMainThread(final boolean includeInvisible)
-    {
-        if ( Looper.getMainLooper().getThread() == Thread.currentThread()) {
-            return execute(includeInvisible);
-        }
+	@SuppressWarnings("rawtypes")
+	public List executeInMainThread(final boolean includeInvisible) {
+		if (Looper.getMainLooper().getThread() == Thread.currentThread()) {
+			return execute(includeInvisible);
+		}
 
-        final AtomicReference<List> result = new AtomicReference<List>();
-        InstrumentationBackend.instrumentation.runOnMainSync(new Runnable() {
-            @Override
-            public void run() {
-                result.set(execute(includeInvisible));
-            }
-        });
-        return result.get();
+		final AtomicReference<List> result = new AtomicReference<List>();
+		final AtomicReference<Throwable> resultErr = new AtomicReference<Throwable>();
+		InstrumentationBackend.instrumentation.runOnMainSync(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					result.set(execute(includeInvisible));
+				} catch (Throwable t) {
+					resultErr.set(t);
+				}
+			}
+		});
+		if (resultErr.get() != null) {
+			return result.get();
+		}
+		throw new RuntimeException(resultErr.get());
 
-    }
+	}
 
-    @SuppressWarnings({"unchecked", "rawtypes" })
+	@SuppressWarnings({ "unchecked", "rawtypes" })
 	public List execute(boolean includeInvisible) {
-        List result = new ArrayList();
-		
-        long before = System.currentTimeMillis();
-        List queryResults = UIQueryEvaluator.evaluateQueryWithOptions(this.queryString, rootViews(), this.arguments);
-        long after = System.currentTimeMillis();
-        
-        String action = "EvaluateQuery";
-        System.out.println(action+ " took: "+ (after-before) + "ms");
-                
-        for (Object v : queryResults) {
-            if (includeInvisible || UIQueryUtils.isVisible(viewFetcher,v)) {
-            	result.add(ViewMapper.extractDataFromView(v));
-            }            
-        }
-               
-        return result;
-    }
+		List result = new ArrayList();
 
+		long before = System.currentTimeMillis();
+		List queryResults = UIQueryEvaluator.evaluateQueryWithOptions(
+				this.queryString, rootViews(), this.arguments);
+		long after = System.currentTimeMillis();
 
-    public List<View> allVisibleViews() {
-        return viewFetcher.getAllViews(false);
-    }
+		String action = "EvaluateQuery";
+		System.out.println(action + " took: " + (after - before) + "ms");
 
-    public List<View> rootViews() {
-    	return Arrays.asList(viewFetcher.getWindowDecorViews());
-    }
+		for (Object v : queryResults) {
+			if (includeInvisible || UIQueryUtils.isVisible(viewFetcher, v)) {
+				result.add(ViewMapper.extractDataFromView(v));
+			}
+		}
+
+		return result;
+	}
+
+	public List<View> allVisibleViews() {
+		return viewFetcher.getAllViews(false);
+	}
+
+	public List<View> rootViews() {
+		return Arrays.asList(viewFetcher.getWindowDecorViews());
+	}
 }
