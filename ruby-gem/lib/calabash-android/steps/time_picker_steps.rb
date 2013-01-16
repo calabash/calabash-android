37334ce1ae90b441a3b510098c5c6a3012eac4c0
Jonas Maturana Larsen <jonasmaturanalarsen@gmail.com>
2013-01-16 07:48:29 +0100

    Fixed arguement order when invoking set_time_with_description

--- ruby-gem/lib/calabash-android/steps/time_picker_steps.rb
+++ ruby-gem/lib/calabash-android/steps/time_picker_steps.rb
@@ -4,5 +4,5 @@ Given /^I set the time to "(\d\d:\d\d)" on TimePicker with index "([^\"]*)"$/ do
 end
 
 Given /^I set the "([^\"]*)" time to "(\d\d:\d\d)"$/ do |content_description, time|
-  performAction('set_time_with_description', time, content_description)
+  performAction('set_time_with_description', content_description, time)
 end
