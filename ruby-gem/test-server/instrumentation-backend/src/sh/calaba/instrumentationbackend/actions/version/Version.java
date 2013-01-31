f6de97fedc933b07be9624c65df29f625e600237
Karl Krukow <karl.krukow@gmail.com>
2013-01-31 11:33:34 +0100

    Document: Version command is GENERATEd - do not edit

--- ruby-gem/test-server/instrumentation-backend/src/sh/calaba/instrumentationbackend/actions/version/Version.java
+++ ruby-gem/test-server/instrumentation-backend/src/sh/calaba/instrumentationbackend/actions/version/Version.java
@@ -4,8 +4,24 @@ import sh.calaba.instrumentationbackend.Result;
 import sh.calaba.instrumentationbackend.actions.Action;
 
 
+/**
+ * Generated Code.
+ * Do not edit.
+ *
+ */
 public class Version implements Action {
 
+	/******
+	 * Generate Version Number
+	 * DO NOT EDIT
+	 * 
+	 * Version number is changed by changing SERVER_VERSION
+	 * in calabash-android/version.rb
+	 * 
+	 * When doing so, this version will be copied here when
+	 * test server is built.
+	 * 
+	 */
 	public static final String VERSION="0.4.0.pre12";
     
 	@Override
