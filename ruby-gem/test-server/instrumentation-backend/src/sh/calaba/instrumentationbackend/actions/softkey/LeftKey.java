a4490b949a6524c8fca4b2cbe2e95e15f3fc8ab9
James Tsay <tjames@15MBP-04307.local>
2013-02-06 13:34:24 -0800

    Add actions: send_key_up, send_key_left, and send_key_right

---
+++ ruby-gem/test-server/instrumentation-backend/src/sh/calaba/instrumentationbackend/actions/softkey/LeftKey.java
@@ -0,0 +1,24 @@
+package sh.calaba.instrumentationbackend.actions.softkey;
+
+
+import com.jayway.android.robotium.solo.Solo;
+
+import sh.calaba.instrumentationbackend.InstrumentationBackend;
+import sh.calaba.instrumentationbackend.Result;
+import sh.calaba.instrumentationbackend.actions.Action;
+
+
+public class LeftKey implements Action {
+
+    @Override
+    public Result execute(String... args) {
+        InstrumentationBackend.solo.sendKey(Solo.LEFT);
+        return Result.successResult();
+    }
+
+    @Override
+    public String key() {
+        return "send_key_left";
+    }
+
+}
---
+++ ruby-gem/test-server/instrumentation-backend/src/sh/calaba/instrumentationbackend/actions/softkey/RightKey.java
@@ -0,0 +1,24 @@
+package sh.calaba.instrumentationbackend.actions.softkey;
+
+
+import com.jayway.android.robotium.solo.Solo;
+
+import sh.calaba.instrumentationbackend.InstrumentationBackend;
+import sh.calaba.instrumentationbackend.Result;
+import sh.calaba.instrumentationbackend.actions.Action;
+
+
+public class RightKey implements Action {
+
+    @Override
+    public Result execute(String... args) {
+        InstrumentationBackend.solo.sendKey(Solo.RIGHT);
+        return Result.successResult();
+    }
+
+    @Override
+    public String key() {
+        return "send_key_right";
+    }
+
+}
