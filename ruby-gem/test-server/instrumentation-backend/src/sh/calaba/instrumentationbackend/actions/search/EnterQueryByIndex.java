96b494db37996ed0725c5d6217e949ccede245b2
streetsofboston <flyingdutchie@gmail.com>
2013-06-07 15:53:08 -0400

    Fix compilation issues with respect to latest ADT version.

--- ruby-gem/test-server/instrumentation-backend/src/sh/calaba/instrumentationbackend/actions/search/EnterQueryByIndex.java
+++ ruby-gem/test-server/instrumentation-backend/src/sh/calaba/instrumentationbackend/actions/search/EnterQueryByIndex.java
@@ -1,10 +1,13 @@
 package sh.calaba.instrumentationbackend.actions.search;
 
+import android.annotation.TargetApi;
+import android.os.Build;
 import android.widget.SearchView;
 import sh.calaba.instrumentationbackend.InstrumentationBackend;
 import sh.calaba.instrumentationbackend.Result;
 import sh.calaba.instrumentationbackend.actions.Action;
 
+@TargetApi(Build.VERSION_CODES.HONEYCOMB)
 public class EnterQueryByIndex implements Action {
     @Override
     public Result execute(String... args) {
