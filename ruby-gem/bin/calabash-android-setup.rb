7cec2c1b84e6bd7e1844153dccf5630ca4ae8c6d
Jonas Maturana Larsen <jonasmaturanalarsen@gmail.com>
2013-09-04 21:53:39 +0200

    calabash_settings is no longer a hidden file

--- ruby-gem/bin/calabash-android-setup.rb
+++ ruby-gem/bin/calabash-android-setup.rb
@@ -13,7 +13,7 @@ def calabash_setup
   ask_for_setting(:keystore_password, "Please enter the password for the keystore")
   ask_for_setting(:keystore_alias, "Please enter the alias")
 
-  open('.calabash_settings', 'w') do |f|
+  open('calabash_settings', 'w') do |f|
     f.puts @settings.to_json
   end
   puts "Saved your settings to .calabash_settings. You can edit the settings manually or run this setup script again"
