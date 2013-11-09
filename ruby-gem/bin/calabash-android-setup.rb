e81e75a3fe067a6e125cb49ac0af215463e9dfc8
Jérémie Laval <jeremie.laval@gmail.com>
2013-11-09 12:54:55 -0500

    [setup] Don't display password entry with setup command

--- ruby-gem/bin/calabash-android-setup.rb
+++ ruby-gem/bin/calabash-android-setup.rb
@@ -1,4 +1,5 @@
 require "calabash-android/version"
+require "io/console"
 
 def calabash_setup
   @settings = {}
@@ -6,7 +7,7 @@ def calabash_setup
   puts "Please enter keystore information to use a custom keystore instead of the default"
 
   ask_for_setting(:keystore_location, "Please enter keystore location")
-  ask_for_setting(:keystore_password, "Please enter the password for the keystore")
+  ask_for_setting(:keystore_password, "Please enter the password for the keystore", true)
   ask_for_setting(:keystore_alias, "Please enter the alias")
 
   open('calabash_settings', 'w') do |f|
@@ -15,7 +16,7 @@ def calabash_setup
   puts "Saved your settings to .calabash_settings. You can edit the settings manually or run this setup script again"
 end
 
-def ask_for_setting(key, msg)
+def ask_for_setting(key, msg, secure = false)
   puts msg
-  @settings[key] = STDIN.gets.chomp
+  @settings[key] = (!secure ? STDIN.gets : STDIN.noecho(&:gets)).chomp
 end
