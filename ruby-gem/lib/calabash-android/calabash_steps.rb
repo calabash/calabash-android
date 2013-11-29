217e99711943b68ec909dbd31f40b7af940db3c8
Jonas Maturana Larsen <jonasmaturanalarsen@gmail.com>
2013-11-29 13:59:04 -0800

    Deleting unused constants
    Conflicts:
    	ruby-gem/lib/calabash-android/calabash_steps.rb

--- ruby-gem/lib/calabash-android/calabash_steps.rb
+++ ruby-gem/lib/calabash-android/calabash_steps.rb
@@ -1,7 +1,3 @@
-
-WAIT_TIMEOUT = (ENV['WAIT_TIMEOUT'] || 30).to_f
-STEP_PAUSE = (ENV['STEP_PAUSE'] || 0.5).to_f
-
 require 'calabash-android/steps/assert_steps'
 require 'calabash-android/steps/check_box_steps'
 require 'calabash-android/steps/context_menu_steps'
