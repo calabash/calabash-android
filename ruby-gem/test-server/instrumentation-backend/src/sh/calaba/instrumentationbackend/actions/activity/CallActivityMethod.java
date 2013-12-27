9dd55f465be93a30467bcace68401002287bd2cd
AlexeyDsov <alexeydsov@gmail.com>
2013-12-27 10:09:35 +0400

    update CallActivityMethod

--- ruby-gem/test-server/instrumentation-backend/src/sh/calaba/instrumentationbackend/actions/activity/CallActivityMethod.java
+++ ruby-gem/test-server/instrumentation-backend/src/sh/calaba/instrumentationbackend/actions/activity/CallActivityMethod.java
@@ -21,9 +21,7 @@ public class CallActivityMethod implements Action {
 
         Activity currentActivity = InstrumentationBackend.solo.getCurrentActivity();
         try {
-            MethodWrapper methodWrapper = getMethod(currentActivity, args[0], argClasses);
-            Object result = methodWrapper.method.invoke(currentActivity, methodWrapper.isStringArray ? new Object[]{callArgs} : callArgs);
-            return processMethodReturn(methodWrapper.method, result);
+            return findMethod(currentActivity, args[0], argClasses).invoke(currentActivity, callArgs);
         } catch (NoSuchMethodException e) {
             return getFailResult("Activity %s does not contain method %s", currentActivity, argClasses, args[0]);
         } catch (IllegalAccessException e) {
@@ -47,38 +45,21 @@ public class CallActivityMethod implements Action {
         }
     }
 
-    private MethodWrapper getMethod(Activity activity, String name, Class[] argClasses) throws NoSuchMethodException {
+    private MethodWrapper findMethod(Activity activity, String name, Class[] argClasses) throws NoSuchMethodException, UnexpectedMethodReturnTypeException {
         Class<? extends Activity> activityClass = activity.getClass();
+        Method method;
+        boolean isArrayArgs;
         try {
-            return new MethodWrapper(activityClass.getMethod(name, argClasses), false);
-        } catch (NoSuchMethodException e) { /* ok */ }
-        try {
-            return new MethodWrapper(activityClass.getMethod(name, new Class[]{String[].class}), true);
-        } catch (NoSuchMethodException e) { /* ok */ }
-
-        throw new NoSuchMethodException();
-    }
-
-    private Result processMethodReturn(Method method, Object invokeReturn) throws UnexpectedMethodReturnTypeException {
-        Class<?> returnType = method.getReturnType();
-        if (returnType.getName().equals("void")) {
-            return Result.successResult();
-        } else if (returnType.getName().equals("boolean") || returnType.equals(Boolean.class)) {
-            if (invokeReturn instanceof Boolean) {
-                Boolean isSuccess = (Boolean) invokeReturn;
-                return new Result(isSuccess, isSuccess ? null : "True expected, got false");
-            }
-            return Result.failedResult("Boolean expected, got null");
-        } else if (returnType.equals(String.class)) {
-            if (invokeReturn instanceof String) {
-                Result result = new Result(true);
-                result.addBonusInformation((String) invokeReturn);
-                return result;
-            }
-            return Result.failedResult("String expected, got null");
-        } else {
-            throw new UnexpectedMethodReturnTypeException(returnType);
+            method = activityClass.getMethod(name, argClasses);
+            isArrayArgs = false;
+        } catch (NoSuchMethodException e) {
+            method = activityClass.getMethod(name, new Class[]{String[].class});
+            isArrayArgs = true;
         }
+
+        ResultConverter resultConverter = new ResultConverterFactory().get(method.getReturnType());
+
+        return new MethodWrapper(method, isArrayArgs, resultConverter);
     }
 
     private Result getFailResult(String msg, Activity currentActivity, Class[] argClasses, String methodName, Object... others) {
@@ -107,13 +88,87 @@ public class CallActivityMethod implements Action {
         return stringBuilder.toString();
     }
 
+    private interface ResultConverter {
+        boolean checkReturnType(Class<?> returnType);
+        Result getResult(Object invokeReturn);
+    }
+
+    private class VoidResultConverter implements ResultConverter {
+        @Override
+        public boolean checkReturnType(Class<?> returnType) {
+            return returnType.getName().equals("void");
+        }
+
+        @Override
+        public Result getResult(Object invokeReturn) {
+            return Result.successResult();
+        }
+    }
+
+    private class BooleanResultConverter implements ResultConverter {
+        @Override
+        public boolean checkReturnType(Class<?> returnType) {
+            return returnType.getName().equals("boolean") || returnType.equals(Boolean.class);
+        }
+
+        @Override
+        public Result getResult(Object invokeReturn) {
+            if (invokeReturn instanceof Boolean) {
+                Boolean isSuccess = (Boolean) invokeReturn;
+                return new Result(isSuccess, isSuccess ? null : "True expected, got false");
+            }
+            return Result.failedResult("Boolean expected, got null");
+        }
+    }
+
+    private class StringResultConverter implements ResultConverter {
+        @Override
+        public boolean checkReturnType(Class<?> returnType) {
+            return returnType.equals(String.class);
+        }
+
+        @Override
+        public Result getResult(Object invokeReturn) {
+            if (invokeReturn instanceof String) {
+                Result result = new Result(true);
+                result.addBonusInformation((String) invokeReturn);
+                return result;
+            }
+            return Result.failedResult("String expected, got null");
+        }
+    }
+
+    private class ResultConverterFactory {
+        private ResultConverter get(Class<?> returnType) throws UnexpectedMethodReturnTypeException {
+            ResultConverter[] list = {
+                new VoidResultConverter(),
+                new BooleanResultConverter(),
+                new StringResultConverter()
+            };
+            for (ResultConverter converter : list) {
+                if (converter.checkReturnType(returnType)) {
+                    return converter;
+                }
+            }
+            throw new UnexpectedMethodReturnTypeException(returnType);
+        }
+    }
+
     private class MethodWrapper {
         private final Method method;
         private final boolean isStringArray;
+        private final ResultConverter resultConverter;
 
-        private MethodWrapper(Method method, boolean isStringArray) {
+        private MethodWrapper(Method method, boolean isStringArray, ResultConverter resultConverter) {
             this.method = method;
             this.isStringArray = isStringArray;
+            this.resultConverter = resultConverter;
+        }
+
+        private Result invoke(Activity activity, String[] callArgs) throws InvocationTargetException, IllegalAccessException {
+            return resultConverter.getResult(
+                method.invoke(activity, isStringArray ? new Object[]{callArgs} : callArgs)
+            );
         }
     }
 
