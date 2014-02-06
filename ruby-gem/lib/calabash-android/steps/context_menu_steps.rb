fc801284cc18b591c79e492714983fdc025f8e9a
Tobias Roikjer <tobias.roikjer@xamarin.com>
2014-02-06 11:08:14 +0100

    Removed press_long_on_text_and_select_with_index action. Changed predefined step.

--- ruby-gem/lib/calabash-android/steps/context_menu_steps.rb
+++ ruby-gem/lib/calabash-android/steps/context_menu_steps.rb
@@ -1,6 +1,10 @@
+Then /^I long press "([^\"]*)" and select item number (\d+)$/ do |text_to_press, index|
+  long_press("* {text CONTAINS '#{text_to_press}'}")
+  wait_for_element_exist("com.android.internal.view.menu.ListMenuItemView")
 
-Then /^I long press "([^\"]*)" and select item number "([^\"]*)"$/ do |text_to_press, index|
-  perform_action('press_long_on_text_and_select_with_index', text_to_press, index)
+  touch_index = index.to_i - 1
+
+  touch("com.android.internal.view.menu.ListMenuItemView android.widget.TextView index:#{touch_index}")
 end
 
 Then /^I long press "([^\"]*)" and select "([^\"]*)"$/ do |text_to_press, context_text|
