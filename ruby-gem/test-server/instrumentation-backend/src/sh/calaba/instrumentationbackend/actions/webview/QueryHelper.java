c46717044bf2efacb7a101607af9f0cdcb20d717
Tobias Roikjer <tobias.roikjer@xamarin.com>
2014-04-28 14:43:53 +0200

    Querying for elements in a webview no longer changes any UI (like hiding the keyboard).

--- ruby-gem/test-server/instrumentation-backend/src/sh/calaba/instrumentationbackend/actions/webview/QueryHelper.java
+++ ruby-gem/test-server/instrumentation-backend/src/sh/calaba/instrumentationbackend/actions/webview/QueryHelper.java
@@ -11,30 +11,32 @@ import java.util.Map;
 import sh.calaba.instrumentationbackend.InstrumentationBackend;
 import sh.calaba.instrumentationbackend.actions.webview.CalabashChromeClient.WebFuture;
 import sh.calaba.org.codehaus.jackson.map.ObjectMapper;
+
+import android.os.Build;
 import android.util.Log;
 import android.webkit.ValueCallback;
 import android.webkit.WebView;
 
 public class QueryHelper {
 
-	
+
 	@SuppressWarnings("unchecked")
 	public static Map<String, Object> findFirstVisibleRectangle(List<HashMap<String,Object>> elements) {
-		return (Map<String, Object>)findFirstVisibleElement(elements).get("rect");	
+		return (Map<String, Object>)findFirstVisibleElement(elements).get("rect");
 	}
-	
+
 	public static Map<String, Object> findFirstVisibleElement(List<HashMap<String,Object>> elements) {
 		//TODO: Should do something more intelligent
-		return (Map<String, Object>)elements.get(0);	
+		return (Map<String, Object>)elements.get(0);
 	}
-	
+
 	public static float translateCoordToScreen(int offset, float scale, Object point) {
 		return offset + ((Number)point).floatValue() *scale;
 	}
-	
+
 	public static Map<String, Object> translateRectToScreenCoordinates(WebView webView, Map<String, Object> rectangle) {
 		try {
-			
+
             float scale = webView.getScale();
 
 			int[] webviewLocation = new int[2];
@@ -45,24 +47,24 @@ public class QueryHelper {
 					rectangle.get("center_x"));
 			float center_y = translateCoordToScreen(webviewLocation[1], scale,
 					rectangle.get("center_y"));
-									
+
 			float x = translateCoordToScreen(webviewLocation[0], scale, rectangle.get("left"));
 			float y = translateCoordToScreen(webviewLocation[0], scale, rectangle.get("top"));
 			Map<String,Object> result = new HashMap<String, Object>(rectangle);
-			
+
 			result.put("x",x);
 			result.put("y",y);
 			result.put("center_x",center_x);
 			result.put("center_y",center_y);
-			
+
 			return result;
-			
-	
+
+
 		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
 	}
-	
+
 	public static String toJsonString(Object o) {
 		//http://www.mkyong.com/java/how-to-convert-java-map-to-from-json-jackson/
 		try {
@@ -71,7 +73,7 @@ public class QueryHelper {
 			throw new RuntimeException(e);
 		}
 	}
-	
+
 
     private static String readJavascriptFromAsset(String scriptPath) {
     	StringBuffer script = new StringBuffer();
@@ -88,7 +90,7 @@ public class QueryHelper {
 		}
 		return script.toString();
     }
-	
+
 	public static WebFuture executeAsyncJavascriptInWebviews(WebView webView,
 		String scriptPath, String selector, String type) {
 
@@ -99,12 +101,12 @@ public class QueryHelper {
 
         CalabashChromeClient chromeClient = CalabashChromeClient.prepareWebView(webView);
 
-        if (android.os.Build.VERSION.SDK_INT < 19) { // Android 4.4
-            webView.loadUrl("javascript:calabash_result = " + script + ";prompt('calabash:' + calabash_result);");
+        if (Build.VERSION.SDK_INT < 19) { // Android 4.4
+            JavaScriptExecuter javaScriptExecuter = new JavaScriptExecuter(webView);
+            javaScriptExecuter.executeJavaScript("calabash_result = " + script + ";prompt('calabash:' + calabash_result);");
         } else {
             chromeClient.evaluateCalabashScript(script);
         }
-
         return chromeClient.getResult();
 	}
 }
