7acdcf7fcf4a6c5c9fece7e83276b4c472c15bcf
Giorgos Saslis <g.saslis@gmail.com>
2014-05-06 18:50:34 +0300

    Assert l10n Text Action
    Added new action for asserting the presence of internationalized text.

---
+++ ruby-gem/test-server/instrumentation-backend/src/sh/calaba/instrumentationbackend/actions/l10n/Assertl10nText.java
@@ -0,0 +1,35 @@
+package sh.calaba.instrumentationbackend.actions.l10n;
+
+import sh.calaba.instrumentationbackend.InstrumentationBackend;
+import sh.calaba.instrumentationbackend.Result;
+import sh.calaba.instrumentationbackend.actions.Action;
+
+public class Assertl10nText extends Action{
+
+
+    @Override
+    public Result execute(String... args) {
+        String l10nKey = args[0];
+        boolean shouldBeFound = Boolean.parseBoolean(args[1]);
+        String pckg = (args.length > 2)? args[2] : null;
+
+        String myLocalizedString = L10nHelper.getValue(l10nKey, pckg);
+
+
+        boolean found = InstrumentationBackend.solo.searchText(myLocalizedString, true);
+
+        if(shouldBeFound && !found) {
+            return new Result(false, "Text'" + myLocalizedString + "' was not found");
+        } else if(!shouldBeFound && found) {
+            return new Result(false, "Text'" + myLocalizedString + "' was found");
+        } else {
+            return Result.successResult();
+        }
+    }
+
+    @Override
+    public String key() {
+        return "assert_l10n_text";
+    }
+
+}
