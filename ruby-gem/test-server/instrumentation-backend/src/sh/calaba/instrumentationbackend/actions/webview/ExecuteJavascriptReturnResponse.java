3337424c8bd8765a11e23d34dc3d046b07b80982
Deepak Rai <deepak.rai@inmobi.com>
2014-06-06 10:58:07 +0530

    Fixing the Indentation

--- ruby-gem/test-server/instrumentation-backend/src/sh/calaba/instrumentationbackend/actions/webview/ExecuteJavascriptReturnResponse.java
+++ ruby-gem/test-server/instrumentation-backend/src/sh/calaba/instrumentationbackend/actions/webview/ExecuteJavascriptReturnResponse.java
@@ -12,16 +12,15 @@ import android.webkit.WebView;
 
 public class ExecuteJavascriptReturnResponse implements Action {
 
-    @SuppressWarnings({ "unchecked", "rawtypes" })
+	@SuppressWarnings({ "unchecked", "rawtypes" })
 	@Override
-    public Result execute(String... args) {
-    	final String scriptCode = args[0]; 
+	public Result execute(String... args) {
+		final String scriptCode = args[0]; 
 		final Integer webviewNumber = Integer.parseInt(args[1]);
+		List<WebFuture> webResults = (List<WebFuture>) UIQueryUtils.evaluateSyncInMainThread(new Callable() {			
 
-    	List<WebFuture> webResults = (List<WebFuture>) UIQueryUtils.evaluateSyncInMainThread(new Callable() {			
-			
-			public Object call() throws Exception {
-				
+		public Object call() throws Exception {
+
 				List<WebFuture> webResults = new ArrayList();
 				List<CalabashChromeClient> list = CalabashChromeClient.findAndPrepareWebViews();
 				if (list.isEmpty()) {
@@ -36,38 +35,38 @@ public class ExecuteJavascriptReturnResponse implements Action {
 
 				WebView webView = ccc.getWebView();
 				final String script = "javascript:(function() {"
-							+ " try { "
-		                    + "prompt('calabash:' + " + scriptCode + ");"
-		                    + " } catch (e) {" 
-		                    + " } "
-		                    + "})()";
+			+ " try { "
+			+ "prompt('calabash:' + " + scriptCode + ");"
+			+ " } catch (e) {" 
+			+ " } "
+			+ "})()";
 
 				System.out.println("execute javascript: " + script);
 
-		        webView.loadUrl(script);
-		        webResults.add(ccc.getResult());				
+				webView.loadUrl(script);
+				webResults.add(ccc.getResult());				
 				return webResults;
 
 			}
 		});
-    	
-    	List<String> allResults = new ArrayList<String>(webResults.size());
-    	for (WebFuture f : webResults) {
-    		allResults.add(f.getAsString());			    		
-    	}
-    		
+
+	List<String> allResults = new ArrayList<String>(webResults.size());
+	for (WebFuture f : webResults) {
+		allResults.add(f.getAsString());			    		
+	}
+
     	if (allResults.size() == 0) {
-    		return new Result(false, "No WebView found");	
-    	}
-    	else {
-    		return new Result(true, allResults);
-    	}
+		return new Result(false, "No WebView found");	
+	}
+	else {
+		return new Result(true, allResults);
+	}
 
     }
 
     @Override
     public String key() {
-        return "execute_javascript_return_response";
+	return "execute_javascript_return_response";
     }
 
 }
