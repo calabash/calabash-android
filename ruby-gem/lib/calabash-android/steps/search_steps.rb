8235a6bc04d7c8e8b1a3d908cf55ac7887c9e67d
Tobias Roikjer <tobias.roikjer@xamarin.com>
2014-06-06 12:40:03 +0200

    Removed enter_query_into_numbered_field. Use enter_text("SearchView index:...", text) instead.

--- ruby-gem/lib/calabash-android/steps/search_steps.rb
+++ ruby-gem/lib/calabash-android/steps/search_steps.rb
@@ -1,7 +1,7 @@
 Then /^I enter "([^\"]*)" into search field$/ do |text|
-  perform_action('enter_query_into_numbered_field', text, 1)
+  enter_text("SearchView index:0", text)
 end
 
 Then /^I enter "([^\"]*)" into search field number (\d+)$/ do |text, number|
-  perform_action('enter_query_into_numbered_field', text, number)
+  enter_text("SearchView index:#{number.to_i-1}", text)
 end
