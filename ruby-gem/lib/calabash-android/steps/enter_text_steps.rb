08264acd2324472f2c4cc0dea27c8d08c2ab192b
Tobias Roikjer <tobias.roikjer@xamarin.com>
2014-06-06 17:55:42 +0200

    Updated enter_text_steps. Used android.widget.EditText instead of EditText.

--- ruby-gem/lib/calabash-android/steps/enter_text_steps.rb
+++ ruby-gem/lib/calabash-android/steps/enter_text_steps.rb
@@ -1,27 +1,27 @@
 Then /^I enter "([^\"]*)" as "([^\"]*)"$/ do |text, content_description|
-  enter_text("EditText contentDescription:'#{content_description}'", text)
+  enter_text("android.widget.EditText contentDescription:'#{content_description}'", text)
 end
 
 Then /^I enter "([^\"]*)" into "([^\"]*)"$/ do |text, content_description|
-  enter_text("EditText {contentDescription LIKE[c] '#{content_description}'}", text)
+  enter_text("android.widget.EditText {contentDescription LIKE[c] '#{content_description}'}", text)
 end
 
 Then /^I enter "([^\"]*)" into input field number (\d+)$/ do |text, index|
-  enter_text("EditText index:#{index.to_i-1}", text)
+  enter_text("android.widget.EditText index:#{index.to_i-1}", text)
 end
 
 Then /^I enter text "([^\"]*)" into field with id "([^\"]*)"$/ do |text, id|
-  enter_text("EditText id:'#{id}'", text)
+  enter_text("android.widget.EditText id:'#{id}'", text)
 end
 
 Then /^I clear "([^\"]*)"$/ do |name|
-  query("EditText {contentDescription LIKE[c] '#{name}'}", setText: '')
+  query("android.widget.EditText {contentDescription LIKE[c] '#{name}'}", setText: '')
 end
 
 Then /^I clear input field number (\d+)$/ do |number|
-  query("EditText index:#{number.to_i-1}", setText: '')
+  query("android.widget.EditText index:#{number.to_i-1}", setText: '')
 end
 
 Then /^I clear input field with id "([^\"]*)"$/ do |view_id|
-  query("EditText id:'#{view_id}'", setText: '')
+  query("android.widget.EditText id:'#{view_id}'", setText: '')
 end
