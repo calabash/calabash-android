afd7f64c400891be968bb907019823bf305fd5ab
Deepak Rai <deepak.rai@inmobi.com>
2014-06-06 11:11:08 +0530

    Fixed indentation

--- ruby-gem/test-server/instrumentation-backend/src/sh/calaba/instrumentationbackend/actions/webview/ExecuteJavascriptOnWebviewNumber.java
+++ ruby-gem/test-server/instrumentation-backend/src/sh/calaba/instrumentationbackend/actions/webview/ExecuteJavascriptOnWebviewNumber.java
@@ -17,16 +17,16 @@ public class ExecuteJavascriptOnWebviewNumber implements Action {
 	public Result execute(String... args) {
 		final String scriptCode = args[0]; 
 		final Integer webviewNumber = Integer.parseInt(args[1]);
-		List<WebFuture> webResults = (List<WebFuture>) UIQueryUtils.evaluateSyncInMainThread(new Callable() {			
-			
+		List<WebFuture> webResults = (List<WebFuture>) UIQueryUtils.evaluateSyncInMainThread(new Callable() {
+
 			public Object call() throws Exception {
-				
+
 				List<WebFuture> webResults = new ArrayList();
 				List<CalabashChromeClient> list = CalabashChromeClient.findAndPrepareWebViews();
 				if (list.isEmpty()) {
 					return webResults;
 				}
-				
+
 				System.out.println("WebView Count : " + list.size());
 				CalabashChromeClient ccc = list.get(0);
 
@@ -35,40 +35,40 @@ public class ExecuteJavascriptOnWebviewNumber implements Action {
 
 				WebView webView = ccc.getWebView();
 				final String script = "javascript:(function() {"
-                        + " var r;"
-                        + " try {"
-                        + "  r = (function() {"
-                        + scriptCode + ";"
-                        + "  }());"
-                        + " } catch (e) {"
-                        + "  r = 'Exception: ' + e;"
-                        + " }"
-                        + " prompt('calabash:'+r);"
-                        + "}())";
+			+ " var r;"
+			+ " try {"
+			+ "  r = (function() {"
+			+ scriptCode + ";"
+			+ "  }());"
+			+ " } catch (e) {"
+			+ "  r = 'Exception: ' + e;"
+			+ " }"
+			+ " prompt('calabash:'+r);"
+			+ "}())";
 
 				System.out.println("execute javascript: " + script);
 
-		        webView.loadUrl(script);
-		        webResults.add(ccc.getResult());				
+				webView.loadUrl(script);
+				webResults.add(ccc.getResult());				
 				return webResults;
-				
+
 			}
 		});
-    	
-    	List<String> allResults = new ArrayList<String>(webResults.size());
-    	boolean success = true;
-    	for (WebFuture f : webResults) {
-    		String result = f.getAsString();
+
+		List<String> allResults = new ArrayList<String>(webResults.size());
+		boolean success = true;
+		for (WebFuture f : webResults) {
+			String result = f.getAsString();
 			allResults.add(result);    		
 			if (result.startsWith("Exception:")) {
 				success = false;
 			}			
-    	}
-    		
-    	if (allResults.size() == 0) {
-    		return new Result(false, "No WebView found");	
-    	}
-    	else {
+		}
+
+		if (allResults.size() == 0) {
+			return new Result(false, "No WebView found");	
+		}
+		else {
 			return new Result(success, allResults);
 		}
 	}
