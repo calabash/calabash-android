6a67669691da09f5662088561c28b6347d3a66a5
Tobias Roikjer <tobias.roikjer@xamarin.com>
2014-06-12 16:16:08 +0200

    Set timeout to 10 seconds in assert_steps

--- ruby-gem/lib/calabash-android/steps/assert_steps.rb
+++ ruby-gem/lib/calabash-android/steps/assert_steps.rb
@@ -1,29 +1,29 @@
 Then /^I see the text "([^\"]*)"$/ do |text|
-  wait_for_text(text)
+  wait_for_text(text, timeout: 10)
 end
 
 Then /^I see "([^\"]*)"$/ do |text|
-  wait_for_text(text)
+  wait_for_text(text, timeout: 10)
 end
 
 Then /^I should see "([^\"]*)"$/ do |text|
-  wait_for_text(text)
+  wait_for_text(text, timeout: 10)
 end
 
 Then /^I should see text containing "([^\"]*)"$/ do |text|
-  wait_for_text(text)
+  wait_for_text(text, timeout: 10)
 end
 
 
 
 Then /^I should not see "([^\"]*)"$/ do |text|
-  wait_for_text_to_disappear(text)
+  wait_for_text_to_disappear(text, timeout: 10)
 end
 
 Then /^I don't see the text "([^\"]*)"$/ do |text|
-  wait_for_text_to_disappear(text)
+  wait_for_text_to_disappear(text, timeout: 10)
 end
 
 Then /^I don't see "([^\"]*)"$/ do |text|
-  wait_for_text_to_disappear(text)
+  wait_for_text_to_disappear(text, timeout: 10)
 end
