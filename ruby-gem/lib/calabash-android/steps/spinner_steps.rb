dcfd867f340e98b8c98553254619287a58d13cd4
Tobias Roikjer <tobias.roikjer@xamarin.com>
2014-06-12 15:14:47 +0200

    Updated spinner_steps. The predefined method acts more like expected.
    It now looks for either a spinner marked by the identifier, or the
    spinners child views marked by the identifier.

--- ruby-gem/lib/calabash-android/steps/spinner_steps.rb
+++ ruby-gem/lib/calabash-android/steps/spinner_steps.rb
@@ -1,14 +1,11 @@
 Then /^I select "([^\"]*)" from "([^\"]*)"$/ do |item_text, spinner_identifier|
-  spinner = query("android.widget.Spinner marked:'#{spinner_identifier}'").first
+  spinner = query("android.widget.Spinner marked:'#{spinner_identifier}'")
 
-  raise 'Spinner not found' unless spinner
-
-  if spinner
+  if spinner.empty?
+    touch_when_element_exists("android.widget.Spinner * marked:'#{spinner_identifier}'")
+  else
     touch(spinner)
-
-    q = "android.widget.PopupWindow$PopupViewContainer android.widget.CheckedTextView marked:'#{item_text}'"
-
-    wait_for_element_exists(q)
-    touch(q)
   end
+
+  touch_when_element_exists("android.widget.PopupWindow$PopupViewContainer * marked:'#{item_text}'")
 end
