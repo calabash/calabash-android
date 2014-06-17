8a42e744463c15efd8e62097dbb112d0370feab2
Tobias Roikjer <tobias.roikjer@xamarin.com>
2014-06-17 16:09:18 +0200

    Updated context_menu_steps to better reflect usage.

--- ruby-gem/lib/calabash-android/steps/context_menu_steps.rb
+++ ruby-gem/lib/calabash-android/steps/context_menu_steps.rb
@@ -1,19 +1,18 @@
-Then /^I long press "([^\"]*)" and select item number (\d+)$/ do |text_to_press, index|
+Then /^I long press "([^\"]*)" and select item number (\d+)$/ do |text, index|
   step_deprecated
 
-  long_press_when_element_exists("* {text CONTAINS '#{text_to_press}'}")
+  long_press_when_element_exists("* {text CONTAINS[c] '#{text}'}")
 
-  touch_index = index.to_i - 1
-  tap_when_element_exists("com.android.internal.view.menu.ListMenuItemView android.widget.TextView index:#{touch_index}")
+  tap_when_element_exists("com.android.internal.view.menu.ListMenuItemView android.widget.TextView index:#{index.to_i - 1}")
 end
 
-Then /^I long press "([^\"]*)" and select "([^\"]*)"$/ do |text_to_press, context_text|
+Then /^I long press "([^\"]*)" and select "([^\"]*)"$/ do |text, identifier|
   step_deprecated
 
-  long_press_when_element_exists("* {text CONTAINS '#{text_to_press}'}")
-  tap_when_element_exists("com.android.internal.view.menu.ListMenuItemView android.widget.TextView marked:'#{context_text}'")
+  long_press_when_element_exists("* {text CONTAINS[c] '#{text}'}")
+  tap_when_element_exists("com.android.internal.view.menu.ListMenuItemView android.widget.TextView marked:'#{identifier}'")
 end
 
-Then /^I long press "([^\"]*)"$/ do |text_to_press|
-  long_press_when_element_exists("* {text CONTAINS '#{text_to_press}'}")
+Then /^I long press "([^\"]*)"$/ do |text|
+  long_press_when_element_exists("* {text CONTAINS[c] '#{text}'}")
 end
