36651b274948d233401b56cb9ae16c12cdb99d2f
Tobias Roikjer <tobias.roikjer@xamarin.com>
2014-06-17 16:25:16 +0200

    Updated press_button_steps to better reflect usage.

--- ruby-gem/lib/calabash-android/steps/press_button_steps.rb
+++ ruby-gem/lib/calabash-android/steps/press_button_steps.rb
@@ -1,17 +1,17 @@
-Given /^I press the "([^\"]*)" button$/ do |button_text|
-  tap_when_element_exists("android.widget.Button {text CONTAINS[c] '#{button_text}'}")
+Given /^I press the "([^\"]*)" button$/ do |text|
+  tap_when_element_exists("android.widget.Button {text CONTAINS[c] '#{text}'}")
 end
 
-Then /^I press button number (\d+)$/ do |button_number|
-  tap_when_element_exists("android.widget.Button index:#{button_number.to_i-1}")
+Then /^I press button number (\d+)$/ do |index|
+  tap_when_element_exists("android.widget.Button index:#{index.to_i-1}")
 end
 
-Then /^I press image button number (\d+)$/ do |button_number|
-  tap_when_element_exists("android.widget.ImageButton index:#{button_number.to_i-1}")
+Then /^I press image button number (\d+)$/ do |index|
+  tap_when_element_exists("android.widget.ImageButton index:#{index.to_i-1}")
 end
 
-Then /^I press view with id "([^\"]*)"$/ do |view_id|
-  tap_when_element_exists("* id:'#{view_id}'")
+Then /^I press view with id "([^\"]*)"$/ do |id|
+  tap_when_element_exists("* id:'#{id}'")
 end
 
 Then /^I press "([^\"]*)"$/ do |identifier|
@@ -19,21 +19,21 @@ Then /^I press "([^\"]*)"$/ do |identifier|
 end
 
 Then /^I click on screen (\d+)% from the left and (\d+)% from the top$/ do |x, y|
-  perform_action('click_on_screen',x, y)
+  perform_action('click_on_screen', x, y)
 end
 
 Then /^I touch the "([^\"]*)" text$/ do |text|
-  tap_when_element_exists("* {text LIKE[c] '#{text}'}")
+  tap_when_element_exists("* {text CONTAINS[c] '#{text}'}")
 end
 
-Then /^I press list item number (\d+)$/ do |line_index|
+Then /^I press list item number (\d+)$/ do |index|
   step_deprecated
 
-  tap_when_element_exists("android.widget.ListView index:0 android.widget.TextView index:#{line_index.to_i-1}")
+  tap_when_element_exists("android.widget.ListView index:0 android.widget.TextView index:#{index.to_i-1}")
 end
 
-Then /^I long press list item number (\d+)$/ do |line_index|
+Then /^I long press list item number (\d+)$/ do |index|
   step_deprecated
 
-  long_press_when_element_exists("android.widget.ListView index:0 android.widget.TextView index:#{line_index.to_i-1}")
+  long_press_when_element_exists("android.widget.ListView index:0 android.widget.TextView index:#{index.to_i-1}")
 end
