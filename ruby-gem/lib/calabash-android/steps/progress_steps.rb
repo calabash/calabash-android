b4927345a832ac6bd615388c2c4bdb1281ff8bda
Tobias Roikjer <tobias.roikjer@xamarin.com>
2014-06-17 16:32:39 +0200

    Updated progress_steps to better reflect usage.

--- ruby-gem/lib/calabash-android/steps/progress_steps.rb
+++ ruby-gem/lib/calabash-android/steps/progress_steps.rb
@@ -1,5 +1,5 @@
 Then /^I wait for progress$/ do
-  wait_for_element_do_not_exist("android.widget.ProgressBar", :timeout => 60)
+  wait_for_element_do_not_exist("android.widget.ProgressBar")
 end
 
 Then /^I wait$/ do
@@ -35,12 +35,12 @@ Then /^I wait up to (\d+) seconds to see "([^\"]*)"$/ do |timeout, text|
   wait_for_text(text, timeout: timeout.to_i)
 end
 
-Then /^I wait for the "([^\"]*)" button to appear$/ do |text|
-  wait_for_element_exists("android.widget.Button marked:'#{text}'");
+Then /^I wait for the "([^\"]*)" button to appear$/ do |identifier|
+  wait_for_element_exists("android.widget.Button marked:'#{identifier}'");
 end
 
 Then /^I wait for the view with id "([^\"]*)" to appear$/ do |id|
-  wait_for_element_exists("* id:'#{id}'", {:timeout => 60})
+  wait_for_element_exists("* id:'#{id}'")
 end
 
 Then /^I wait for the "([^\"]*)" view to appear$/ do |text|
@@ -48,20 +48,20 @@ Then /^I wait for the "([^\"]*)" view to appear$/ do |text|
 end
 
 
-Then /^I wait for the "([^\"]*)" screen to appear$/ do |text|
-  wait_for_activity(text)
+Then /^I wait for the "([^\"]*)" screen to appear$/ do |activity_name|
+  wait_for_activity(activity_name)
 end
 
-Then /^I wait upto (\d+) seconds for the "([^\"]*)" screen to appear$/ do |timeout, text|
-  wait_for_activity(text, timeout: timeout.to_i)
+Then /^I wait upto (\d+) seconds for the "([^\"]*)" screen to appear$/ do |timeout, activity_name|
+  wait_for_activity(activity_name, timeout: timeout.to_i)
 end
 
-Then /^I wait up to (\d+) seconds for the "([^\"]*)" screen to appear$/ do |timeout, text|
-  wait_for_activity(text, timeout: timeout.to_i)
+Then /^I wait up to (\d+) seconds for the "([^\"]*)" screen to appear$/ do |timeout, activity_name|
+  wait_for_activity(activity_name, timeout: timeout.to_i)
 end
 
 # @param - the "tag" associated with the tab, or the text within the tab label
-Then /^I wait for the "([^\"]*)" tab to appear$/ do | tab |
+Then /^I wait for the "([^\"]*)" tab to appear$/ do |tab|
   wait_for do
     query("android.widget.TabWidget descendant TextView {text LIKE[c] '#{tab}'}", :isSelected).first
   end
