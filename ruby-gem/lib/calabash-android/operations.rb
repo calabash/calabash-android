7cb19be057423263815c2767f6f2bb6d4ba648a9
Tobias Roikjer <tobias.roikjer@xamarin.com>
2015-01-06 16:42:31 +0100

    Change signature of start_test_server to match new app class

--- ruby-gem/lib/calabash-android/operations.rb
+++ ruby-gem/lib/calabash-android/operations.rb
@@ -559,7 +559,7 @@ module Calabash module Android
         raise "Could not push #{local} to #{remote}" unless system(cmd)
       end
 
-      def start_test_server(application, test_server, options={})
+      def start_test_server(application, options={})
         raise "Will not start test server because of previous failures." if ::Cucumber.wants_to_quit
 
         if keyguard_enabled?
@@ -581,7 +581,7 @@ module Calabash module Android
           cmd_arr << val.to_s
         end
 
-        cmd_arr << "#{test_server.package_name}/sh.calaba.instrumentationbackend.CalabashInstrumentationTestRunner"
+        cmd_arr << "#{application.test_server.package_name}/sh.calaba.instrumentationbackend.CalabashInstrumentationTestRunner"
 
         cmd = cmd_arr.join(" ")
 
