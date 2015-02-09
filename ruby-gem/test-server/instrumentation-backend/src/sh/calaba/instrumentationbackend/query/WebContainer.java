dc9fd9541ae75051f4c3c0bff7b53322980e0aeb
Tobias Roikjer <tobias.roikjer@xamarin.com>
2015-02-09 17:09:19 +0100

    important

--- ruby-gem/test-server/instrumentation-backend/src/sh/calaba/instrumentationbackend/query/WebContainer.java
+++ ruby-gem/test-server/instrumentation-backend/src/sh/calaba/instrumentationbackend/query/WebContainer.java
@@ -112,7 +112,11 @@ public class WebContainer {
                         getMethod("setJavaScriptEnabled", boolean.class);
 
                 methodSetJavaScriptEnabled.invoke(xWalkSettings, true);
-            } catch (ReflectiveOperationException e) {
+            } catch (InvocationTargetException e) {
+                throw new RuntimeException(e);
+            } catch (NoSuchMethodException e) {
+                throw new RuntimeException(e);
+            } catch (IllegalAccessException e) {
                 throw new RuntimeException(e);
             }
 
@@ -135,7 +139,11 @@ public class WebContainer {
                 });
 
                 return webFuture;
-            } catch (ReflectiveOperationException e) {
+            } catch (InvocationTargetException e) {
+                throw new RuntimeException(e);
+            } catch (NoSuchMethodException e) {
+                throw new RuntimeException(e);
+            } catch (IllegalAccessException e) {
                 throw new RuntimeException(e);
             }
         } else {
