2597ebcf8831ef3ed40acc9c5451c1bb7f742fd1
Tobias Roikjer <tobias.roikjer@xamarin.com>
2015-02-17 16:19:52 +0100

    Ops: Add method to obtain last broadcast intent

---
+++ ruby-gem/lib/calabash-android/android_component.rb
@@ -0,0 +1,23 @@
+module Calabash
+  module Android
+    class AndroidComponent
+      attr_reader :package_name, :class_name
+
+      def initialize(package_name, class_name)
+        @package_name = package_name
+        @class_name = class_name
+      end
+
+      def self.from_json(json)
+        Component.new(json['packageName'], json['className'])
+      end
+
+      def to_json(*a)
+        {
+            'className' => class_name,
+            'packageName' => package_name
+        }.to_json
+      end
+    end
+  end
+end
