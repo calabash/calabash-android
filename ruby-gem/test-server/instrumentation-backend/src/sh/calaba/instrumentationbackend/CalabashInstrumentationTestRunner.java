dfd1162a4b3f20d55b1f602ca03a13aa4e9ff85c
Tobias Roikjer <tobias.roikjer@xamarin.com>
2015-02-23 17:53:54 +0100

    TS: Automatic default of main_activity if none is given

--- ruby-gem/test-server/instrumentation-backend/src/sh/calaba/instrumentationbackend/CalabashInstrumentationTestRunner.java
+++ ruby-gem/test-server/instrumentation-backend/src/sh/calaba/instrumentationbackend/CalabashInstrumentationTestRunner.java
@@ -5,13 +5,48 @@ import java.lang.reflect.Method;
 import android.app.Activity;
 import sh.calaba.instrumentationbackend.actions.HttpServer;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.test.InstrumentationTestRunner;
 
 public class CalabashInstrumentationTestRunner extends InstrumentationTestRunner {
 	@Override
     public void onCreate(Bundle arguments) {
+        final String mainActivity;
+
+        if (arguments.containsKey("main_activity")) {
+            mainActivity = arguments.getString("main_activity");
+        } else {
+            PackageManager packageManager = getTargetContext().getPackageManager();
+            Intent launchIntent =
+                    packageManager.getLaunchIntentForPackage(arguments.getString("target_package"));
+            String mainActivityTmpName = launchIntent.getComponent().getClassName();
+
+            try {
+                PackageInfo packageInfo = packageManager.getPackageInfo(arguments.getString("target_package"),
+                        PackageManager.GET_ACTIVITIES);
+                ActivityInfo[] activityInfoArr = packageInfo.activities;
+
+                for (ActivityInfo activityInfo : activityInfoArr) {
+                    if (activityInfo.name.equals(mainActivityTmpName) &&
+                            activityInfo.targetActivity != null) {
+                        mainActivityTmpName = activityInfo.targetActivity;
+                        break;
+                    }
+                }
+
+                mainActivity = mainActivityTmpName;
+            } catch (PackageManager.NameNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+
+            System.out.println("Main activity name automatically set to: " + mainActivity);
+        }
+
 		try {
 			Context context = getTargetContext();
 			Class<?> c = Class.forName("mono.MonoPackageManager");
@@ -25,7 +60,7 @@ public class CalabashInstrumentationTestRunner extends InstrumentationTestRunner
                 loadApplication.invoke (null, context, null, strings);
             }
 			System.out.println("Calabash loaded Mono");
-            InstrumentationBackend.mainActivity = Class.forName(arguments.getString("main_activity")).asSubclass(Activity.class);
+            InstrumentationBackend.mainActivity = Class.forName(mainActivity).asSubclass(Activity.class);
 		} catch (Exception e) {
 			System.out.println("Calabash did not load Mono. This is only a problem if you are trying to test a Mono application");
 		}
@@ -48,7 +83,7 @@ public class CalabashInstrumentationTestRunner extends InstrumentationTestRunner
         InstrumentationBackend.extras = extras;
 
         try {
-            InstrumentationBackend.mainActivityName = arguments.getString("main_activity");
+            InstrumentationBackend.mainActivityName = mainActivity;
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
