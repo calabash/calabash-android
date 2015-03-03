b4105381070086dc4b937919f37237c79c2d6756
Tobias Roikjer <tobias.roikjer@xamarin.com>
2015-03-03 13:09:34 +0100

    TS: Remove uses of ReflectiveOperationException

--- ruby-gem/test-server/instrumentation-backend/src/sh/calaba/instrumentationbackend/intenthook/InstrumentationHook.java
+++ ruby-gem/test-server/instrumentation-backend/src/sh/calaba/instrumentationbackend/intenthook/InstrumentationHook.java
@@ -8,6 +8,7 @@ import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.IBinder;
 
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 import sh.calaba.instrumentationbackend.InstrumentationBackend;
@@ -74,7 +75,11 @@ public class InstrumentationHook extends IntentHook {
             return new IntentHookResult(null, true);
         } catch (ClassNotFoundException e) {
             throw new RuntimeException(e);
-        } catch (ReflectiveOperationException e) {
+        } catch (InvocationTargetException e) {
+            throw new RuntimeException(e);
+        } catch (NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        } catch (IllegalAccessException e) {
             throw new RuntimeException(e);
         }
     }
