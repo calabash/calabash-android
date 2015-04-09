18067a38548370b28f0176632a3c89a7fe580c51
Simon Søndergaard <simon.sondergaard@xamarin.com>
2015-04-09 16:39:26 +0200

    remove duplicates

--- ruby-gem/test-server/instrumentation-backend/src/sh/calaba/instrumentationbackend/query/ast/UIQueryUtils.java
+++ ruby-gem/test-server/instrumentation-backend/src/sh/calaba/instrumentationbackend/query/ast/UIQueryUtils.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -86,6 +87,11 @@ public class UIQueryUtils {
 		return controls;
 
 	}
+
+    public static List uniq(List list) {
+        return new ArrayList(new LinkedHashSet(list));
+    }
+
 
 	@SuppressWarnings({ "unchecked", "rawtypes" })
 	public static List parents(Object o) {
