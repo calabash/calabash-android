c266ce72144b72fafd7b0a542045c4aa774e7dd4
Tobias Roikjer <tobias.roikjer@xamarin.com>
2015-04-16 12:07:02 +0200

    IH: Add one more execStartActivity hookable event

--- ruby-gem/test-server/instrumentation-backend/src/sh/calaba/instrumentationbackend/intenthook/IIntentHook.java
+++ ruby-gem/test-server/instrumentation-backend/src/sh/calaba/instrumentationbackend/intenthook/IIntentHook.java
@@ -1,6 +1,7 @@
 package sh.calaba.instrumentationbackend.intenthook;
 
 import android.app.Activity;
+import android.app.Fragment;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,5 +9,8 @@ import android.os.IBinder;
 
 public interface IIntentHook {
     public IntentHookResult execStartActivity(Context who, IBinder contextThread, IBinder token, Activity target,
+                                                       Intent intent, int requestCode, Bundle options);
+
+    public IntentHookResult execStartActivity(Context who, IBinder contextThread, IBinder token, Fragment target,
                                                        Intent intent, int requestCode, Bundle options);
 }
