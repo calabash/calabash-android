202a7cb9b9527050bd95f409ae1df9163fd966e2
Tobias Roikjer <tobias.roikjer@xamarin.com>
2015-04-20 14:05:59 +0200

    Client: Fix: hashname spelling

--- ruby-gem/lib/calabash-android/operations.rb
+++ ruby-gem/lib/calabash-android/operations.rb
@@ -1231,7 +1231,7 @@ module Calabash module Android
       result = JSON.parse(http("/last-broadcast-intent"))
 
       unless result['outcome'] == 'SUCCESS'
-        raise "/last-broadcast-intent failed because: #{result['reason']}\n#{result['details']}"
+        raise "/last-broadcast-intent failed because: #{result['reason']}\n#{result['detail']}"
       end
 
       json = JSON.parse(result['result'])
@@ -1247,7 +1247,7 @@ module Calabash module Android
       result = JSON.parse(http('/intent-hook', intent_hook))
 
       if result['outcome'] && result['outcome'] != 'SUCCESS'
-        raise "/intent-hook failed because: #{result['reason']}\n#{result['details']}"
+        raise "/intent-hook failed because: #{result['reason']}\n#{result['detail']}"
       end
     end
 
