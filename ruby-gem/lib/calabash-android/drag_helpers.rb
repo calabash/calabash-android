279308a0004126bbeb4787f0be5f7acf090167c6
Christopher Fuentes <chris.fuentes@xamarin.com>
2015-07-20 08:17:06 -0700

    drag_helpers: Refactored for XTC

--- ruby-gem/lib/calabash-android/drag_helpers.rb
+++ ruby-gem/lib/calabash-android/drag_helpers.rb
@@ -3,29 +3,18 @@ require 'calabash-android/env'
 module Calabash
   module Android
     module DragHelpers
+      include Calabash::Android::EnvironmentHelpers
 
-      # Public API
-      def drag_coordinates(from_x, from_y, to_x, to_y, options={})
-        puts "Dragging from #{from_x},#{from_y} to #{to_x},#{to_y}"
+      MAX_RETRIES = 10
+      @@monkey_port = nil
+      @@monkey_pid = nil
 
-        monkey_touch(:down, from_x, from_y)
-        sleep(options.fetch(:hold_time, 1))
-
-        x_delta = to_x - from_x
-        y_delta = to_y - from_y
-        steps = options.fetch(:steps, 5)
-
-        (1..steps).each do |i|
-          move_x = (from_x + (i * (x_delta.to_f / steps))).to_i
-          move_y = (from_y + (i * (y_delta.to_f / steps))).to_i
-          monkey_touch(:move, move_x, move_y)
-        end
-
-        sleep(options.fetch(:hang_time, 1))
-        monkey_touch(:up, to_x, to_y)
+      def drag_coordinates(from_x, from_y, to_x, to_y, steps=10, hold_time=0.5, hang_time=0.5)
+        log "Dragging from #{from_x},#{from_y} to #{to_x},#{to_y}"
+        _monkey_move_from(from_x, from_y, to_x, to_y, hold_time: hold_time, hang_time: hang_time, steps: steps)
       end
 
-      def drag_and_drop(from_query, to_query, options={})
+      def drag_and_drop(from_query, to_query, steps=10, hold_time=0.5, hang_time=0.5)
         wait_for_element_exists(from_query)
         wait_for_element_exists(to_query)
 
@@ -36,56 +25,75 @@ module Calabash
           if to_results.any?
             from_rect = from_results.first['rect']
             to_rect = to_results.first['rect']
-            drag_coordinates(from_rect['center_x'], from_rect['center_y'], to_rect['center_x'], to_rect['center_y'], options)
+            from_x = from_rect['center_x']
+            from_y = from_rect['center_y']
+            to_x = to_rect['center_x']
+            to_y = to_rect['center_y']
+
+            _monkey_move_from(from_x, from_y, to_x, to_y, hold_time: hold_time, hang_time: hang_time, steps: steps)
           else
-            puts "No matching elements for: #{to_query}"
+            raise "No matching elements for: #{to_query}"
           end
         else
-          puts "No matching elements for: #{from_query}"
+          raise "No matching elements for: #{from_query}"
         end
       end
 
       private
-      MAX_RETRIES = 7
-      @@monkey_port = nil
 
-      def start_monkey
-        kill_existing_monkey_processes
-        MAX_RETRIES.times do
-          port = rand((1025..65535))
-          output = ''
-          timeout(0.5) do
-            output = `#{adb_command} shell monkey --port #{port}`
-          end rescue Timeout::Error #ignore timeout error
+      def _monkey_move_from(from_x, from_y, to_x, to_y, args={})
+        @@monkey_port = _start_monkey
+        _monkey_touch(:down, from_x, from_y)
+        sleep(args.fetch(:hold_time))
 
-          unless output.include? 'Error binding to network socket.'
-            #The shim will always exit, but on success it will not output the line above
-            if existing_monkey_pids.count == 1 then
-              return port
-            end
-          end
+        x_delta = to_x - from_x
+        y_delta = to_y - from_y
+        steps = args.fetch(:steps)
+
+        steps.times do |i|
+          move_x = (from_x + ((i+1) * (x_delta.to_f / steps))).to_i
+          move_y = (from_y + ((i+1) * (y_delta.to_f / steps))).to_i
+          _monkey_touch(:move, move_x, move_y)
         end
-        raise 'Unable to connect to start com.android.commands.monkey'
+
+        sleep(args.fetch(:hang_time))
+        _monkey_touch(:up, to_x, to_y)
       end
 
-      def existing_monkey_pids
-        procs = `#{adb_command} shell ps`
+      def _start_monkey
+        unless xamarin_test_cloud? #monkey processes are automatically killed in XTC
+          _kill_existing_monkey_processes
+        end
+
+        MAX_RETRIES.times do
+          port = rand((1024..65535))
+
+          monkey_starter_thread = Thread.new do
+            Thread.current[:output]= `#{_adb_command} shell monkey --port #{port}`
+          end
+          sleep(4)
+
+          output = monkey_starter_thread[:output]
+          unless output && output.include?('Error binding to network socket.')
+            return port
+          end
+        end
+        raise 'Unable to start com.android.commands.monkey'
+      end
+
+      def _existing_monkey_pids
+        procs = `#{_adb_command} shell ps`
         procs.scan(/.+?\s(?<pid>[0-9]+).+?com.android.commands.monkey\r?\n?/).flatten
       end
 
-      def kill_existing_monkey_processes
-        existing_monkey_pids.each do |pid|
-          `#{adb_command} shell kill -9 #{pid}`
+      def _kill_existing_monkey_processes
+          _existing_monkey_pids.each do |pid|
+            `#{_adb_command} shell kill -9 #{pid}`
+          end
         end
       end
 
-      def ensure_connected
-        #lazy instantiation of monkey, since it takes a while
-        @@monkey_port ||= start_monkey
-      end
-
-      def monkey_touch(touch_type, x, y)
-        ensure_connected
+      def _monkey_touch(touch_type, x, y)
         case touch_type
           when :move
             perform_action('send_tcp', @@monkey_port, "touch move #{x} #{y}", true)
@@ -98,16 +106,12 @@ module Calabash
         end
       end
 
-      #private version of Operations Device.adb_command
-      def adb_command
-        @operations_instance ||= lambda do
-          Class.new do
-            include Calabash::Android::Operations
-          end.new
-        end.call
-
-        device = @operations_instance.default_device
-        device.send(:adb_command)
+      def _adb_command
+        if xamarin_test_cloud?
+          Env.adb_path
+        else
+          default_device.adb_command
+        end
       end
     end
   end
