1b917e42021c23feeb4f2c7fab9bd7c25af341f8
Joshua Moody <joshuajmoody@gmail.com>
2016-06-24 15:23:12 +0200

    Travis: ensure AndroidManifest exist before bundle update

--- bin/ci/travis/run.sh
+++ bin/ci/travis/run.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
 cd ruby-gem
-bundle update
 
 touch lib/calabash-android/lib/TestServer.apk
+touch lib/calabash-android/lib/AndroidManifest.xml
+
+bundle update
+
 gem build calabash-android.gemspec
 
 bundle exec rake unit
