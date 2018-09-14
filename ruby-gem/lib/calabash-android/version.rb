22b1f5df572124d8430e858a1560deba4315f892
Rajdeep Varma <mail.rajvarma@gmail.com>
2018-09-14 13:44:53 +0100

    [uiautomator2] update version with proper uaitomator instance creation in server

--- ruby-gem/lib/calabash-android/version.rb
+++ ruby-gem/lib/calabash-android/version.rb
@@ -1,7 +1,7 @@
 module Calabash
   module Android
-    VERSION = "0.9.6.uia1"
+    VERSION = "0.9.6.uia2"
   end
 end
 
-# Server from Branch uiautomator2#02b031a
+# Server from Branch uiautomator2#c7de735d402e58b208321d906884d0e7ddd787eb
