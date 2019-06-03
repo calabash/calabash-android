f67b7fd4f260d65fff7529dd06c69bd4949d9993
Joshua Moody <josmoo@microsoft.com>
2019-06-03 13:42:36 +0200

    gem: set version to 0.9.10.pre1

--- ruby-gem/lib/calabash-android/version.rb
+++ ruby-gem/lib/calabash-android/version.rb
@@ -1,6 +1,6 @@
 module Calabash
   module Android
-    VERSION = "0.9.10-1"
+    VERSION = "0.9.10.pre1"
 
     # A model of a software release version that can be used to compare two versions.
     #
