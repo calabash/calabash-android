377a72efb9119659f5d69d094f738fd899b604f1
Rajdeep Varma <mail.rajvarma@gmail.com>
2019-08-12 14:42:03 +0100

    [uiautomator2] new version

--- ruby-gem/lib/calabash-android/version.rb
+++ ruby-gem/lib/calabash-android/version.rb
@@ -1,6 +1,7 @@
 module Calabash
   module Android
-    VERSION = "0.9.12"
+    VERSION = "0.9.12.uia1"
+    # Server Commit a43c9b3b0efa84721a9ed4fc36052cf7f502cfd7
 
     # A model of a software release version that can be used to compare two versions.
     #
