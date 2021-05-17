c9435c1f625055deec0637491638e827594aa611
Serghei Moret <joesatriani323@gmail.com>
2021-05-17 14:52:00 +0200

    Update helpers.rb

--- ruby-gem/lib/calabash-android/helpers.rb
+++ ruby-gem/lib/calabash-android/helpers.rb
@@ -170,12 +170,12 @@ def fingerprint_from_apk(app_path)
     Dir.chdir(tmp_dir) do
       FileUtils.cp(app_path, "app.apk")
       FileUtils.mkdir("META-INF")
-require 'pry'
-      binding.pry
+
       Calabash::Utils.with_silent_zip do
         Zip::File.foreach("app.apk") do |z|
           #if /^META-INF\/\w+\.(rsa|dsa)$/i =~ z.name
-            extract(z.path) { z }
+          log 'gotcha' if File.exists?("app.apk")  
+          extract("app.apk") { z }
           #end
         end
       end
